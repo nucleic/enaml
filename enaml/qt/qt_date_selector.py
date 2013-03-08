30733884eeca37b931700e69ef3070ef58b82958
S. Chris Colbert <sccolbert@gmail.com>
2013-03-08 14:39:35 -0500

    Update the DateSelector definitions

--- enaml/qt/qt_date_selector.py
+++ enaml/qt/qt_date_selector.py
@@ -5,94 +5,91 @@
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #------------------------------------------------------------------------------
-from .qt.QtGui import QDateEdit
-from .qt_bounded_date import QtBoundedDate
+from PyQt4.QtGui import QDateEdit
+
+from atom.api import Typed
+
+from enaml.widgets.date_selector import ProxyDateSelector
+
+from .qt_bounded_date import QtBoundedDate, CHANGED_GUARD, as_qdate, as_pydate
 
 
-class QtDateSelector(QtBoundedDate):
-    """ A Qt implementation of an Enaml DateSelector.
+class QtDateSelector(QtBoundedDate, ProxyDateSelector):
+    """ A Qt implementation of an Enaml ProxyDateSelector.
 
     """
+    #: A reference to the widget created by the proxy.
+    widget = Typed(QDateEdit)
+
     #--------------------------------------------------------------------------
-    # Setup Methods
+    # Initialization API
     #--------------------------------------------------------------------------
-    def create_widget(self, parent, tree):
-        """ Create the underlying QDateEdit widget.
+    def create_widget(self):
+        """ Create the QDateEdit widget.
 
         """
-        return QDateEdit(parent)
+        self.widget = QDateEdit(self.parent_widget())
 
-    def create(self, tree):
-        """ Create and initialize the underlying widget.
-
-        """
-        super(QtDateSelector, self).create(tree)
-        self.set_date_format(tree['date_format'])
-        self.set_calendar_popup(tree['calendar_popup'])
-        self.widget().dateChanged.connect(self.on_date_changed)
-
-    #--------------------------------------------------------------------------
-    # Message Handling
-    #--------------------------------------------------------------------------
-    def on_action_set_date_format(self, content):
-        """ Handle the 'set_date_format' action from the Enaml widget.
+    def init_widget(self):
+        """ Initialize the widget.
 
         """
-        self.set_date_format(content['date_format'])
-
-    def on_action_set_calendar_popup(self, content):
-        """ Handle the 'set_calendar_popup' action from the Enaml widget.
-
-        """
-        self.set_calendar_popup(content['calendar_popup'])
+        super(QtDateSelector, self).init_widget()
+        d = self.declaration
+        self.set_date_format(d.date_format)
+        self.set_calendar_popup(d.calendar_popup)
+        self.widget.dateChanged.connect(self.on_date_changed)
 
     #--------------------------------------------------------------------------
-    # Widget Update Methods
+    # Abstract API Implementation
     #--------------------------------------------------------------------------
     def get_date(self):
         """ Return the current date in the control.
 
         Returns
         -------
-        result : QDate
-            The current control date as a QDate object.
+        result : date
+            The current control date as a date object.
 
         """
-        return self.widget().date()
+        return as_pydate(self.widget.date())
+
+    def set_minimum(self, date):
+        """ Set the widget's minimum date.
+
+        Parameters
+        ----------
+        date : date
+            The date object to use for setting the minimum date.
+
+        """
+        self.widget.setMinimumDate(as_qdate(date))
+
+    def set_maximum(self, date):
+        """ Set the widget's maximum date.
+
+        Parameters
+        ----------
+        date : date
+            The date object to use for setting the maximum date.
+
+        """
+        self.widget.setMaximumDate(as_qdate(date))
 
     def set_date(self, date):
         """ Set the widget's current date.
 
         Parameters
         ----------
-        date : QDate
-            The QDate object to use for setting the date.
+        date : date
+            The date object to use for setting the date.
 
         """
-        with self.loopback_guard('date'):
-            self.widget().setDate(date)
-
-    def set_max_date(self, date):
-        """ Set the widget's maximum date.
-
-        Parameters
-        ----------
-        date : QDate
-            The QDate object to use for setting the maximum date.
-
-        """
-        self.widget().setMaximumDate(date)
-
-    def set_min_date(self, date):
-        """ Set the widget's minimum date.
-
-        Parameters
-        ----------
-        date : QDate
-            The QDate object to use for setting the minimum date.
-
-        """
-        self.widget().setMinimumDate(date)
+        self._guard |= CHANGED_GUARD
+        try:
+            self.widget.setDate(as_qdate(date))
+        finally:
+            self._guard &= ~CHANGED_GUARD
 
     def set_date_format(self, date_format):
         """ Set the widget's date format.
@@ -105,7 +102,7 @@ class QtDateSelector(QtBoundedDate):
         """
         # XXX make sure Python's and Qt's format strings are the
         # same, or convert between the two.
-        self.widget().setDisplayFormat(date_format)
+        self.widget.setDisplayFormat(date_format)
 
     def set_calendar_popup(self, popup):
         """ Set whether a calendar popup is available on the widget.
@@ -116,5 +113,4 @@ class QtDateSelector(QtBoundedDate):
             Whether the calendar popup is enabled.
 
         """
-        self.widget().setCalendarPopup(popup)
-
+        self.widget.setCalendarPopup(popup)
