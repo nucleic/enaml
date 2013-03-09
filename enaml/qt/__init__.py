fe2aa422b31cbdb5fa3010b51e29f5524722c05e
S. Chris Colbert <sccolbert@gmail.com>
2013-03-09 12:07:52 -0500

    Set the sip api to version 2 when the /qt subpackage is imported

--- enaml/qt/__init__.py
+++ enaml/qt/__init__.py
@@ -5,3 +5,11 @@
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #------------------------------------------------------------------------------
+import sip
+sip.setapi('QDate', 2)
+sip.setapi('QDateTime', 2)
+sip.setapi('QString', 2)
+sip.setapi('QTextStream', 2)
+sip.setapi('QTime', 2)
+sip.setapi('QUrl', 2)
+sip.setapi('QVariant', 2)
