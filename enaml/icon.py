729e53f96d2c891e9155ab4fc8e1510e24b3f1f2
S. Chris Colbert <sccolbert@gmail.com>
2013-03-10 15:21:49 -0400

    Update the Image and Icon objects

--- enaml/icon.py
+++ enaml/icon.py
@@ -5,7 +5,7 @@
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #------------------------------------------------------------------------------
-from atom.api import Atom, Enum, Typed, List
+from atom.api import Atom, Enum, Typed, List, Value
 
 from .image import Image
 
@@ -13,9 +13,9 @@ from .image import Image
 class IconImage(Atom):
     """ An object representing an image in an icon.
 
-    Instances of this class are used to populate the `images` list of
-    an `Icon` instance. Instances of this class should be treated as
-    read-only once they are created.
+    Instances of this class are used to populate the 'images' list of
+    an 'Icon' instance. Instances of this class should be treated as
+    read only once they are created.
 
     """
     #: The widget mode for which this icon should apply.
@@ -31,6 +31,14 @@ class IconImage(Atom):
 class Icon(Atom):
     """ An object object representing an icon.
 
+    Once an image is created it should be treated as read only. User
+    code should create a new icon object if the parameters need to
+    be changed.
+
     """
     #: The list of icon images which compose this icon.
     images = List(IconImage)
+
+    #: Storage space for use by a toolkit backend to use as needed.
+    #: This should not typically be manipulated by user code.
+    _tkdata = Value()
--- enaml/image.py
+++ enaml/image.py
@@ -5,13 +5,17 @@
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #------------------------------------------------------------------------------
-from atom.api import Atom, Enum, Str, Coerced
+from atom.api import Atom, Enum, Str, Value, Coerced
 
 from enaml.layout.geometry import Size
 
 
 class Image(Atom):
     """ An object representing an image.
+
+    Once an image is created it should be treated as read only. User
+    code should create a new image object if the parameters need to
+    be changed.
 
     """
     #: The format of the image. By default, the consumer of the image
@@ -32,9 +36,21 @@ class Image(Atom):
     )
 
     #: The (width, height) size of the image. An invalid size indicates
-    #: that the size of the image should be automatically inferred.
+    #: that the size of the image should be automatically inferred. A
+    #: valid size indicates that the toolkit image should be scaled to
+    #: the specified size.
     size = Coerced(Size, (-1, -1))
+
+    #: The aspect ratio mode to use when the toolkit scales the image.
+    aspect_ratio_mode = Enum('ignore', 'keep', 'keep_by_expanding')
+
+    #: The transform mode to use when the toolkit scales the image.
+    transform_mode = Enum('smooth', 'fast')
 
     # XXX this needs to be augmented to support arrays.
     #: The bytestring holding the data for the image.
     data = Str()
+
+    #: Storage space for use by a toolkit backend to use as needed.
+    #: This should not typically be manipulated by user code.
+    _tkdata = Value()
