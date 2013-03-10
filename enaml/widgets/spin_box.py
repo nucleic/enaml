b79ba79726ede7605461ac2bb2369de71a017858
S. Chris Colbert <sccolbert@gmail.com>
2013-03-10 19:38:43 -0400

    Update the SpinBox definitions

--- enaml/widgets/spin_box.py
+++ enaml/widgets/spin_box.py
@@ -5,11 +5,48 @@
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #------------------------------------------------------------------------------
-from atom.api import Int, Bool, Range, Unicode, observe, set_default
+from atom.api import (
+    Int, Bool, Range, Unicode, Typed, ForwardTyped, observe, set_default
+)
 
 from enaml.core.declarative import d_
 
-from .control import Control
+from .control import Control, ProxyControl
+
+
+class ProxySpinBox(ProxyControl):
+    """ The abstract defintion of a proxy SpinBox object.
+
+    """
+    #: A reference to the SpinBox declaration.
+    declaration = ForwardTyped(lambda: SpinBox)
+
+    def set_minimum(self, minimum):
+        raise NotImplementedError
+
+    def set_maximum(self, maximum):
+        raise NotImplementedError
+
+    def set_value(self, value):
+        raise NotImplementedError
+
+    def set_prefix(self, prefix):
+        raise NotImplementedError
+
+    def set_suffix(self, suffix):
+        raise NotImplementedError
+
+    def set_special_value_text(self, text):
+        raise NotImplementedError
+
+    def set_single_step(self, step):
+        raise NotImplementedError
+
+    def set_read_only(self, read_only):
+        raise NotImplementedError
+
+    def set_wrapping(self, wrapping):
+        raise NotImplementedError
 
 
 class SpinBox(Control):
@@ -22,9 +59,9 @@ class SpinBox(Control):
     #: The maximum value for the spin box. Defaults to 100.
     maximum = d_(Int(100))
 
-    #: The current integer value for the spin box, constrained to
-    #: minimum <= value <= maximum.
-    value = d_(Range('minimum', 'maximum'))
+    #: The position value of the spin box. The value will be clipped to
+    #: always fall between the minimum and maximum.
+    value = d_(Int(0))
 
     #: An optional prefix to include in the displayed text.
     prefix = d_(Unicode())
@@ -52,40 +89,55 @@ class SpinBox(Control):
     #: A spin box expands freely in width by default.
     hug_width = set_default('ignore')
 
-    #--------------------------------------------------------------------------
-    # Messenger API
-    #--------------------------------------------------------------------------
-    def snapshot(self):
-        """ Return the dict of creation attributes for the control.
+    #: A reference to the ProxySpinBox object.
+    proxy = Typed(ProxySpinBox)
 
-        """
-        snap = super(SpinBox, self).snapshot()
-        snap['maximum'] = self.maximum
-        snap['minimum'] = self.minimum
-        snap['value'] = self.value
-        snap['prefix'] = self.prefix
-        snap['suffix'] = self.suffix
-        snap['special_value_text'] = self.special_value_text
-        snap['single_step'] = self.single_step
-        snap['read_only'] = self.read_only
-        snap['wrapping'] = self.wrapping
-        return snap
-
-    @observe(r'^(minimum|maximum|value|prefix|suffix|special_value_text|'
-             r'single_step|read_only|wrapping)$', regex=True)
-    def send_member_change(self, change):
-        """ An observer which sends state change to the client.
+    #--------------------------------------------------------------------------
+    # Observers
+    #--------------------------------------------------------------------------
+    @observe(('minimum', 'maximum', 'value', 'prefix', 'suffix',
+        'special_value_text', 'single_step', 'read_only', 'wrapping'))
+    def _update_proxy(self, change):
+        """ An observer which sends state change to the proxy.
 
         """
         # The superclass handler implementation is sufficient.
-        super(SpinBox, self).send_member_change(change)
+        super(SpinBox, self)._update_proxy(change)
 
     #--------------------------------------------------------------------------
-    # Message Handling
+    # PostSetAttr Handlers
     #--------------------------------------------------------------------------
-    def on_action_value_changed(self, content):
-        """ Handle the 'value_changed' action from the client widget.
+    def _post_setattr_minimum(self, old, new):
+        """ Post setattr the minimum value for the spin box.
+
+        If the new minimum is greater than the current value or maximum,
+        those values are adjusted up.
 
         """
-        self.set_guarded(value=content['value'])
+        if new > self.maximum:
+            self.maximum = new
+        if new > self.value:
+            self.value = new
 
+    def _post_setattr_maximum(self, old, new):
+        """ Post setattr the maximum value for the spin box.
+
+        If the new maximum is less than the current value or the minimum,
+        those values are adjusted down.
+
+        """
+        if new < self.minimum:
+            self.minimum = new
+        if new < self.value:
+            self.value = new
+
+    #--------------------------------------------------------------------------
+    # Post Validation Handlers
+    #--------------------------------------------------------------------------
+    def _post_validate_value(self, old, new):
+        """ Post validate the value for the spin box.
+
+        The value is clipped to minimum and maximum bounds.
+
+        """
+        return max(self.minimum, min(new, self.maximum))
