f0eb6d8417d6ae00af1b00c465e6e47fd46d59b6
Chris Colbert <sccolbert@gmail.com>
2013-03-12 18:57:58 -0400

    bump microversion. The 8 version go last in translation to new org.

--- enaml/version.py
+++ enaml/version.py
@@ -22,7 +22,7 @@ MINOR = 6
 
 # The micro release number. The micro release number is incremented
 # for bug fix releases and small feature additions.
-MICRO = 7
+MICRO = 9
 
 # The version info for the current release.
 version_info = namedtuple('version_info', 'major minor micro')
