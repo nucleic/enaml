044f053a94f4bbfb7a1c490be7c83b0c3f0bda67
Chris Colbert <sccolbert@gmail.com>
2013-03-13 18:43:31 -0400

    Add a simple table widget

---
+++ enaml/qt/qt_table_view.py
@@ -0,0 +1,52 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2013, Nucleic Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#------------------------------------------------------------------------------
+from PyQt4.QtGui import QTableView
+
+from atom.api import Typed
+
+from enaml.widgets.table_view import ProxyTableView
+
+from .q_item_model_wrapper import QItemModelWrapper
+from .qt_control import QtControl
+
+
+class QtTableView(QtControl, ProxyTableView):
+    """ A Qt implementation of an Enaml ProxyTableView.
+
+    """
+    #: A reference to the widget created by the proxy.
+    widget = Typed(QTableView)
+
+    #--------------------------------------------------------------------------
+    # Initialization API
+    #--------------------------------------------------------------------------
+    def create_widget(self):
+        """ Create the underlying widget.
+
+        """
+        self.widget = QTableView(self.parent_widget())
+
+    def init_widget(self):
+        """ Create and initialize the underlying control.
+
+        """
+        super(QtTableView, self).init_widget()
+        d = self.declaration
+        self.set_item_model(d.item_model)
+
+    #--------------------------------------------------------------------------
+    # ProxyListView API
+    #--------------------------------------------------------------------------
+    def set_item_model(self, model):
+        """ Set the item model for the widget.
+
+        """
+        if not model:
+            self.widget.setModel(None)
+        else:
+            self.widget.setModel(QItemModelWrapper(model))
