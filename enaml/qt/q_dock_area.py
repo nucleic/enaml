05616d08a4f563d3e4ffd0ad59d26e042b35d7ba
Chris Colbert <sccolbert@gmail.com>
2013-04-12 16:52:47 -0400

    make dock guide animated

--- enaml/qt/q_dock_area.py
+++ enaml/qt/q_dock_area.py
@@ -5,7 +5,7 @@
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #------------------------------------------------------------------------------
-from PyQt4.QtCore import Qt, QSize, QPoint, QRect, QTimer
+from PyQt4.QtCore import Qt, QSize, QPoint, QRect, QTimer, QPropertyAnimation
 from PyQt4.QtGui import (
     QFrame, QLayout, QRubberBand, QSplitter, QSplitterHandle, QStackedWidget,
     QTabWidget
@@ -341,8 +341,8 @@ class QDockAreaLayout(QLayout):
         hovered = self.hitTest(pos)
         if not isinstance(hovered, (QTabWidget, QDockItem)):
             return False
-        self._prep_item_plug(item, True)
         if hovered is self._root:
+            self._prep_item_plug(item, True)
             sp = self._createSplitter(Qt.Vertical)
             sp.addWidget(item)
             sp.addWidget(self._root)
@@ -350,7 +350,12 @@ class QDockAreaLayout(QLayout):
             sp.setParent(self.parentWidget())
             sp.show()
         else:
-            parent = hovered.parent()  # must be a splitter
+            # hovered parent should logically be a splitter, but the
+            # assumption may break down during very heavy docking.
+            parent = hovered.parent()
+            if not isinstance(parent, QSplitter):
+                return False
+            self._prep_item_plug(item, True)
             index = parent.indexOf(hovered)
             if parent.orientation() == Qt.Vertical:
                 parent.insertWidget(index, item)
@@ -367,8 +372,8 @@ class QDockAreaLayout(QLayout):
         hovered = self.hitTest(pos)
         if not isinstance(hovered, (QTabWidget, QDockItem)):
             return False
-        self._prep_item_plug(item, True)
         if hovered is self._root:
+            self._prep_item_plug(item, True)
             sp = self._createSplitter(Qt.Horizontal)
             sp.addWidget(self._root)
             sp.addWidget(item)
@@ -376,7 +381,12 @@ class QDockAreaLayout(QLayout):
             sp.setParent(self.parentWidget())
             sp.show()
         else:
-            parent = hovered.parent()  # must be a splitter
+            # hovered parent should logically be a splitter, but the
+            # assumption may break down during very heavy docking.
+            parent = hovered.parent()
+            if not isinstance(parent, QSplitter):
+                return False
+            self._prep_item_plug(item, True)
             index = parent.indexOf(hovered)
             if parent.orientation() == Qt.Horizontal:
                 parent.insertWidget(index + 1, item)
@@ -393,8 +403,8 @@ class QDockAreaLayout(QLayout):
         hovered = self.hitTest(pos)
         if not isinstance(hovered, (QTabWidget, QDockItem)):
             return False
-        self._prep_item_plug(item, True)
         if hovered is self._root:
+            self._prep_item_plug(item, True)
             sp = self._createSplitter(Qt.Vertical)
             sp.addWidget(self._root)
             sp.addWidget(item)
@@ -402,7 +412,12 @@ class QDockAreaLayout(QLayout):
             sp.setParent(self.parentWidget())
             sp.show()
         else:
-            parent = hovered.parent()  # must be a splitter
+            # hovered parent should logically be a splitter, but the
+            # assumption may break down during very heavy docking.
+            parent = hovered.parent()
+            if not isinstance(parent, QSplitter):
+                return False
+            self._prep_item_plug(item, True)
             index = parent.indexOf(hovered)
             if parent.orientation() == Qt.Vertical:
                 parent.insertWidget(index + 1, item)
@@ -419,8 +434,8 @@ class QDockAreaLayout(QLayout):
         hovered = self.hitTest(pos)
         if not isinstance(hovered, (QTabWidget, QDockItem)):
             return False
-        self._prep_item_plug(item, True)
         if hovered is self._root:
+            self._prep_item_plug(item, True)
             sp = self._createSplitter(Qt.Horizontal)
             sp.addWidget(item)
             sp.addWidget(self._root)
@@ -428,7 +443,12 @@ class QDockAreaLayout(QLayout):
             sp.setParent(self.parentWidget())
             sp.show()
         else:
-            parent = hovered.parent()  # must be a splitter
+            # hovered parent should logically be a splitter, but the
+            # assumption may break down during very heavy docking.
+            parent = hovered.parent()
+            if not isinstance(parent, QSplitter):
+                return False
+            self._prep_item_plug(item, True)
             index = parent.indexOf(hovered)
             if parent.orientation() == Qt.Horizontal:
                 parent.insertWidget(index, item)
@@ -627,14 +647,22 @@ class QDockAreaLayout(QLayout):
 
         # Splitter handles have priority. Their active area is smaller
         # and overlaps that of other widgets. Giving dock items priority
-        # would make it very difficult to hit a splitter reliably.
+        # would make it very difficult to hit a splitter reliably. In
+        # certain configurations, there may be more than one handle in
+        # the hit box. In that case, the one closest to center wins.
+        handles = []
         for sp in self._splitters:
             pt = sp.mapFrom(widget, pos)
             for index in xrange(1, sp.count()):  # handle 0 is always hidden
                 handle = sp.handle(index)
                 rect = handle.rect().adjusted(-20, -20, 20, 20)
-                if rect.contains(handle.mapFrom(sp, pt)):
-                    return handle
+                pt2 = handle.mapFrom(sp, pt)
+                if rect.contains(pt2):
+                    l = (rect.center() - pt2).manhattanLength()
+                    handles.append((l, handle))
+        if len(handles) > 0:
+            handles.sort()
+            return handles[0][1]
 
         # Check for tab widgets next. A tab widget holds dock items,
         # but should have priority over the dock items themselves.
@@ -727,6 +755,24 @@ class DockOverlays(Atom):
     a more fluid user interaction experience.
 
     """
+    #: The size of the rubber band when docking on the border, in px.
+    BORDER_DOCK_SIZE = 60
+
+    #: The delay to use when triggering the rose timer, in ms.
+    ROSE_DELAY = 30
+
+    #: The delay to use when triggering the band timer, in ms.
+    BAND_DELAY = 50
+
+    #: The target opacity to use when making the band visible.
+    BAND_TARGET_OPACITY = 0.6
+
+    #: The duration of the band visibilty animation, in ms.
+    BAND_VIS_DURATION = 100
+
+    #: the duration of the band geometry animation, in ms.
+    BAND_GEO_DURATION = 100
+
     #: The dock area owner of the overlays.
     _owner = ForwardTyped(lambda: QDockArea)
 
@@ -736,11 +782,20 @@ class DockOverlays(Atom):
     #: The overlayed rubber band.
     _band = Typed(QRubberBand, (QRubberBand.Rectangle,))
 
+    #: The property animator for the rubber band geometry.
+    _geo_animator = Typed(QPropertyAnimation)
+
+    #: The property animator for the rubber band visibility.
+    _vis_animator = Typed(QPropertyAnimation)
+
     #: The target mode to apply to the rose on timeout.
     _target_rose_mode = Int(QGuideRose.Mode.NoMode)
 
     #: The target geometry to apply to rubber band on timeout.
     _target_band_geo = Typed(QRect, factory=lambda: QRect())
+
+    #: The queued band geo.
+    _queued_band_geo = Typed(QRect)
 
     #: The value of the last guide which was hit in the rose.
     _last_guide = Int(-1)
@@ -786,6 +841,23 @@ class DockOverlays(Atom):
         timer.timeout.connect(self._on_band_timer)
         return timer
 
+    def _default__geo_animator(self):
+        """ Create the default property animator for the rubber band.
+
+        """
+        p = QPropertyAnimation(self._band, 'geometry')
+        p.setDuration(self.BAND_GEO_DURATION)
+        return p
+
+    def _default__vis_animator(self):
+        """ Create the default property animator for the rubber band.
+
+        """
+        p = QPropertyAnimation(self._band, 'windowOpacity')
+        p.setDuration(self.BAND_VIS_DURATION)
+        p.finished.connect(self._on_vis_finished)
+        return p
+
     #--------------------------------------------------------------------------
     # Timer Handlers
     #--------------------------------------------------------------------------
@@ -799,7 +871,7 @@ class DockOverlays(Atom):
         rose = self._rose
         rose.setMode(self._target_rose_mode)
         rose.hover(self._hover_pos)
-        self._refresh_band()
+        self._update_band_state()
 
     def _on_band_timer(self):
         """ Handle the timeout event for the internal band timer.
@@ -807,24 +879,88 @@ class DockOverlays(Atom):
         This handler updates the position of the rubber band.
 
         """
-        self._refresh_band()
+        self._update_band_state()
+
+    #--------------------------------------------------------------------------
+    # Animation Handlers
+    #--------------------------------------------------------------------------
+    def _on_vis_finished(self):
+        """ Handle the 'finished' signal from the visibility animator.
+
+        This handle will hide the rubber band when its opacity is 0.
+
+        """
+        band = self._band
+        if band.windowOpacity() == 0.0:
+            band.hide()
 
     #--------------------------------------------------------------------------
     # Private API
     #--------------------------------------------------------------------------
-    def _refresh_band(self):
+    def _update_band_state(self):
         """ Refresh the geometry and visible state of the rubber band.
 
+        The state will be updated using animated properties to provide
+        a nice fluid user experience.
+
         """
+        # A valid geometry indicates that the rubber should be shown on
+        # the screen. An invalid geometry means it should be hidden. If
+        # the validity is changed during animation, the animators are
+        # restarted using the current state as their starting point.
         band = self._band
         geo = self._target_band_geo
         if geo.isValid():
-            band.setGeometry(geo)
+            # If the band is already hidden, the geometry animation can
+            # be bypassed since the band can be located anywhere. The
+            # rose must be raised because QRubberBand raises itself
+            # when it receives a showEvent.
             if band.isHidden():
-                band.show()
+                band.setGeometry(geo)
+                self._start_vis_animator(self.BAND_TARGET_OPACITY)
                 self._rose.raise_()
+                return
+            self._start_vis_animator(self.BAND_TARGET_OPACITY)
+            self._start_geo_animator(geo)
         else:
-            band.hide()
+            self._start_vis_animator(0.0)
+
+    def _start_vis_animator(self, opacity):
+        """ (Re)start the visibility animator.
+
+        Parameters
+        ----------
+        opacity : float
+            The target opacity of the target object.
+
+        """
+        animator = self._vis_animator
+        if animator.state() == animator.Running:
+            animator.stop()
+        target = animator.targetObject()
+        if target.isHidden() and opacity != 0.0:
+            target.setWindowOpacity(0.0)
+            target.show()
+        animator.setStartValue(target.windowOpacity())
+        animator.setEndValue(opacity)
+        animator.start()
+
+    def _start_geo_animator(self, geo):
+        """ (Re)start the visibility animator.
+
+        Parameters
+        ----------
+        geo : QRect
+            The target geometry for the target object.
+
+        """
+        animator = self._geo_animator
+        if animator.state() == animator.Running:
+            animator.stop()
+        target = animator.targetObject()
+        animator.setStartValue(target.geometry())
+        animator.setEndValue(geo)
+        animator.start()
 
     def _band_geometry(self, guide, widget):
         """ Compute the rubber band geometry for a guide and a widget.
@@ -840,7 +976,7 @@ class DockOverlays(Atom):
             will be a splitter handle, a dock item, or a tab widget.
 
         """
-        border_size = 60
+        border_size = self.BORDER_DOCK_SIZE
         owner = self._owner
         Guide = QGuideRose.Guide
         if guide == Guide.NoGuide:
@@ -975,7 +1111,7 @@ class DockOverlays(Atom):
                     self._target_band_geo = g
                 else:
                     self._target_band_geo = QRect()
-                self._band_timer.start(50)
+                self._band_timer.start(self.BAND_DELAY)
             if rose.isHidden():
                 rose.setGeometry(QRect(origin, owner.size()))
                 rose.show()
@@ -1010,7 +1146,7 @@ class DockOverlays(Atom):
             rose.setMode(target_mode)
         elif target_mode != rose.mode():
             rose.setMode(Mode.NoMode)
-            self._rose_timer.start(30)
+            self._rose_timer.start(self.ROSE_DELAY)
         rose.setCenter(center)
 
         # Hit test the rose and update the target geometry for the
@@ -1029,7 +1165,7 @@ class DockOverlays(Atom):
         if rose.mode() != Mode.NoMode:
             rose.hover(pos)
             if update_band:
-                self._band_timer.start(50)
+                self._band_timer.start(self.BAND_DELAY)
 
         # Ensure that the rose is shown on the screen.
         if rose.isHidden():
