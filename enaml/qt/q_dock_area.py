535dd2eee3194c6a5b5da6ca9dbfd180c6eeeed2
Chris Colbert <sccolbert@gmail.com>
2013-04-17 20:02:44 -0400

    Simplify the QDockAreaLayout and move it to the QDockArea module.

--- enaml/qt/q_dock_area.py
+++ enaml/qt/q_dock_area.py
@@ -5,10 +5,131 @@
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #------------------------------------------------------------------------------
-from PyQt4.QtCore import QMargins
+from PyQt4.QtCore import QMargins, QSize
 from PyQt4.QtGui import QFrame, QLayout
 
-from .q_dock_area_layout import QDockAreaLayout
+
+class QDockAreaLayout(QLayout):
+    """ A custom QLayout which is part of the dock area implementation.
+
+    """
+    def __init__(self, parent=None):
+        """ Initialize a QDockAreaLayout.
+
+        Parameters
+        ----------
+        parent : QWidget or None
+            The parent of the layout.
+
+        """
+        super(QDockAreaLayout, self).__init__(parent)
+        self._layout_widget = None
+
+    #--------------------------------------------------------------------------
+    # Public API
+    #--------------------------------------------------------------------------
+    def layoutWidget(self):
+        """ Get the widget implementing the layout for the area.
+
+        Returns
+        -------
+        result : QWidget or None
+            The widget implementing the area, or None if no widget is
+            installed.
+
+        """
+        return self._layout_widget
+
+    def setLayoutWidget(self, widget):
+        """ Set the layout widget for the dock area.
+
+        This method is called by the dock manager which handles the
+        dock area. It should not normally be called by user code.
+
+        Parameters
+        ----------
+        widget : QWidget
+            The widget which implements the dock area layout.
+
+        """
+        old_widget = self._layout_widget
+        if old_widget is not None:
+            old_widget.hide()
+            old_widget.setParent(None)
+        self._layout_widget = widget
+        area = self.parentWidget()
+        if widget is not None:
+            widget.setParent(area)
+            if not area.isHidden():
+                widget.show()
+
+    def setGeometry(self, rect):
+        """ Sets the geometry of all the items in the layout.
+
+        """
+        super(QDockAreaLayout, self).setGeometry(rect)
+        widget = self._layout_widget
+        if widget is not None:
+            widget.setGeometry(self.contentsRect())
+
+    def sizeHint(self):
+        """ Get the size hint for the layout.
+
+        """
+        widget = self._layout_widget
+        if widget is not None:
+            return widget.sizeHint()
+        return QSize(256, 192)
+
+    def minimumSize(self):
+        """ Get the minimum size of the layout.
+
+        """
+        widget = self._layout_widget
+        if widget is not None:
+            return widget.minimumSizeHint()
+        return QSize(256, 192)
+
+    def maximumSize(self):
+        """ Get the maximum size for the layout.
+
+        """
+        widget = self._layout_widget
+        if widget is not None:
+            return widget.maximumSize()
+        return QSize(16777215, 16777215)
+
+    #--------------------------------------------------------------------------
+    # QLayout Abstract API
+    #--------------------------------------------------------------------------
+    def addItem(self, item):
+        """ A required virtual method implementation.
+
+        This method should not be used. Use `setDockLayout` instead.
+
+        """
+        msg = 'Use `setDockLayout` instead.'
+        raise NotImplementedError(msg)
+
+    def count(self):
+        """ A required virtual method implementation.
+
+        This method should not be used and returns a constant value.
+
+        """
+        return 0
+
+    def itemAt(self, idx):
+        """ A virtual method implementation which returns None.
+
+        """
+        return None
+
+    def takeAt(self, idx):
+        """ A virtual method implementation which does nothing.
+
+        """
+        return None
 
 
 class QDockArea(QFrame):
@@ -69,30 +190,3 @@ class QDockArea(QFrame):
                 background: rgb(41, 56, 85);
             }
             """)
-
-    #--------------------------------------------------------------------------
-    # Public API
-    #--------------------------------------------------------------------------
-    def dockLayout(self):
-        """ Get the dock layout for the dock area.
-
-        Returns
-        -------
-        result : DockLayoutBase or None
-            The dock layout for the dock area, or None.
-
-        """
-        return self.layout().dockLayout()
-
-    def setDockLayout(self, layout):
-        """ Set the dock layout for the dock area.
-
-        The old layout will be unparented and hidden, but not destroyed.
-
-        Parameters
-        ----------
-        layout : DockLayoutBase
-            The dock layout node to use for the dock area.
-
-        """
-        self.layout().setDockLayout(layout)
