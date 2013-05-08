e2b3e1efc0e05a511094404801fcb32658f063b5
Chris Colbert <sccolbert@gmail.com>
2013-05-08 19:39:48 -0400

    Update the dock title bar icons

--- enaml/qt/docking/q_dock_title_bar.py
+++ enaml/qt/docking/q_dock_title_bar.py
@@ -12,6 +12,9 @@ from .q_icon_button import QIconButton
 from .q_icon_widget import QIconWidget
 from .q_text_label import QTextLabel
 
+# Make sure the resources get registered.
+from . import dock_resources
+
 
 class IDockTitleBar(QWidget):
     """ An interface class for defining a title bar.
@@ -26,7 +29,7 @@ class IDockTitleBar(QWidget):
     #: A signal emitted when the close button is clicked.
     closeButtonClicked = pyqtSignal()
 
-    #: Show no buttons in the title bar.
+    #: Do not show any buttons in the title bar.
     NoButtons = 0x0
 
     #: Show the maximize button in the title bar.
@@ -159,24 +162,23 @@ class QDockTitleBar(QFrame, IDockTitleBar):
         title_icon.setVisible(False)
 
         title_label = self._title_label = QTextLabel(self)
-        title_label.setText('foo' * 12)
 
         max_icon = QIcon()
         max_icon.addFile(':dock_images/maxbtn_s.png')
         max_icon.addFile(':dock_images/maxbtn_h.png', mode=QIcon.Active)
         max_icon.addFile(':dock_images/maxbtn_p.png', mode=QIcon.Selected)
 
-        rstr_icon = QIcon()
-        rstr_icon.addFile(':dock_images/rstrbtn_s.png')
-        rstr_icon.addFile(':dock_images/rstrbtn_h.png', mode=QIcon.Active)
-        rstr_icon.addFile(':dock_images/rstrbtn_p.png', mode=QIcon.Selected)
+        restore_icon = QIcon()
+        restore_icon.addFile(':dock_images/rstrbtn_s.png')
+        restore_icon.addFile(':dock_images/rstrbtn_h.png', mode=QIcon.Active)
+        restore_icon.addFile(':dock_images/rstrbtn_p.png', mode=QIcon.Selected)
 
         close_icon = QIcon()
         close_icon.addFile(':dock_images/closebtn_s.png')
         close_icon.addFile(':dock_images/closebtn_h.png', mode=QIcon.Active)
         close_icon.addFile(':dock_images/closebtn_p.png', mode=QIcon.Selected)
 
-        btn_size = QSize(14, 14)
+        btn_size = QSize(14, 13)
 
         max_button = self._max_button = QIconButton(self)
         max_button.setIcon(max_icon)
@@ -184,7 +186,7 @@ class QDockTitleBar(QFrame, IDockTitleBar):
         max_button.setVisible(self._buttons & self.MaximizeButton)
 
         restore_button = self._restore_button = QIconButton(self)
-        restore_button.setIcon(rstr_icon)
+        restore_button.setIcon(restore_icon)
         restore_button.setIconSize(btn_size)
         restore_button.setVisible(self._buttons & self.RestoreButton)
 
@@ -196,6 +198,7 @@ class QDockTitleBar(QFrame, IDockTitleBar):
         layout = QHBoxLayout()
         layout.setContentsMargins(QMargins(0, 0, 0, 0))
         layout.setSpacing(1)
+
         layout.addWidget(title_icon)
         layout.addSpacing(0)
         layout.addWidget(title_label, 1)
