dcca1295d80da1c6ab0c9a0e0ba414ea8118f803
Chris Colbert <sccolbert@gmail.com>
2013-05-19 18:05:43 -0400

    Add a utils module with a style repolish function

---
+++ enaml/qt/docking/utils.py
@@ -0,0 +1,21 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2013, Nucleic Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#------------------------------------------------------------------------------
+
+
+def repolish(widget):
+    """ Repolish a widget when the stylesheet dependencies change.
+
+    Parameters
+    ----------
+    widget : QWidget
+        The widget to repolish.
+
+    """
+    style = widget.style()
+    style.unpolish(widget)
+    style.polish(widget)
