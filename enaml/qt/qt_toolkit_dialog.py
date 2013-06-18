4558e3947b606fb5d0d3abd58f63fdf2fe28af5e
Chris Colbert <sccolbert@gmail.com>
2013-06-18 13:10:55 -0400

    add a base class for toolkit dialogs.

---
+++ enaml/qt/qt_toolkit_dialog.py
@@ -0,0 +1,80 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2013, Nucleic Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#------------------------------------------------------------------------------
+from PyQt4.QtGui import QDialog
+
+from atom.api import Typed
+
+from enaml.widgets.toolkit_dialog import ProxyToolkitDialog
+
+from .qt_toolkit_object import QtToolkitObject
+
+
+class QtToolkitDialog(QtToolkitObject, ProxyToolkitDialog):
+    """ A Qt implementation of an Enaml ProxyToolkitDialog.
+
+    """
+    #: A reference to the widget created by the proxy.
+    widget = Typed(QDialog)
+
+    def create_widget(self):
+        """ Create the underlying QColorDialog.
+
+        """
+        self.widget = QDialog(self.parent_widget())
+
+    def init_widget(self):
+        """ Initialize the underlying widget.
+
+        """
+        super(QtToolkitDialog, self).init_widget()
+        self.set_title(self.declaration.title)
+        self.widget.finished.connect(self.on_finished)
+
+    #--------------------------------------------------------------------------
+    # Signal Handlers
+    #--------------------------------------------------------------------------
+    def on_finished(self, result):
+        """ Handle the 'finished' signal from the widget.
+
+        """
+        d = self.declaration
+        if d is not None:
+            d._proxy_finished(bool(result))
+
+    #--------------------------------------------------------------------------
+    # ProxyToolkitDialog API
+    #--------------------------------------------------------------------------
+    def set_title(self, title):
+        """ Set the window title for the underlying widget.
+
+        """
+        self.widget.setWindowTitle(title)
+
+    def show(self):
+        """ Open the dialog as non modal.
+
+        """
+        self.widget.show()
+
+    def open(self):
+        """ Open the dialog as window modal.
+
+        """
+        self.widget.open()
+
+    def exec_(self):
+        """ Open the dialog as application modal.
+
+        """
+        self.widget.exec_()
+
+    def done(self, result):
+        """ Close the dialog with the given result.
+
+        """
+        self.widget.done(int(result))
---
+++ enaml/widgets/toolkit_dialog.py
@@ -0,0 +1,175 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2013, Nucleic Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#------------------------------------------------------------------------------
+from atom.api import (
+    Bool, Unicode, Typed, ForwardTyped, Event, Callable, observe
+)
+
+from enaml.application import deferred_call
+from enaml.core.declarative import d_
+
+from .toolkit_object import ToolkitObject, ProxyToolkitObject
+
+
+class ProxyToolkitDialog(ProxyToolkitObject):
+    """ The abstract defintion of a proxy ToolkitDialog object.
+
+    """
+    #: A reference to the ToolkitDialog declaration.
+    declaration = ForwardTyped(lambda: ToolkitDialog)
+
+    def set_title(self, title):
+        raise NotImplementedError
+
+    def show(self):
+        raise NotImplementedError
+
+    def open(self):
+        raise NotImplementedError
+
+    def exec_(self):
+        raise NotImplementedError
+
+    def done(self, result):
+        raise NotImplementedError
+
+
+class ToolkitDialog(ToolkitObject):
+    """ A base class for defining toolkit dialogs.
+
+    A toolkit dialog is a dialog where the content is defined by the
+    toolkit rather than the user. Customary examples would be a file
+    dialog or a color selection dialog, where the implementation can
+    often be a native operating system dialog.
+
+    """
+    #: The title of the dialog window.
+    title = d_(Unicode())
+
+    #: An optional callback which will be invoked when the dialog is
+    #: closed. This is a convenience to make it easier to handle a
+    #: dialog opened in non-blocking mode. The callback must accept
+    #: a single argument, which will be the dialog instance.
+    callback = d_(Callable())
+
+    #: Whether to destroy the dialog widget on close. The default is
+    #: True since dialogs are typically used in a transitory fashion.
+    destroy_on_close = d_(Bool(True))
+
+    #: An event fired if the dialog is accepted. It has no payload.
+    accepted = d_(Event(), writable=False)
+
+    #: An event fired when the dialog is rejected. It has no payload.
+    rejected = d_(Event(), writable=False)
+
+    #: An event fired when the dialog is finished. The payload is the
+    #: boolean result of the dialog.
+    finished = d_(Event(bool), writable=False)
+
+    #: Whether or not the dialog was accepted by the user. It will be
+    #: updated when the dialog is closed. This value is output only.
+    result = Bool(False)
+
+    #: A reference to the ProxyToolkitDialog object.
+    proxy = Typed(ProxyToolkitDialog)
+
+    def show(self):
+        """ Open the dialog as a non modal dialog.
+
+        """
+        self.result = False
+        if not self.is_initialized:
+            self.initialize()
+        if not self.proxy_is_active:
+            self.activate_proxy()
+        self.proxy.show()
+
+    def open(self):
+        """ Open the dialog as a window modal dialog.
+
+        """
+        self.result = False
+        if not self.is_initialized:
+            self.initialize()
+        if not self.proxy_is_active:
+            self.activate_proxy()
+        self.proxy.open()
+
+    def exec_(self):
+        """ Open the dialog as an application modal dialog.
+
+        Returns
+        -------
+        result : bool
+            Whether or not the dialog was accepted.
+
+        """
+        self.result = False
+        if not self.is_initialized:
+            self.initialize()
+        if not self.proxy_is_active:
+            self.activate_proxy()
+        self.proxy.exec_()
+        return self.result
+
+    def accept(self):
+        """ Accept the current state and close the dialog.
+
+        """
+        self.done(True)
+
+    def reject(self):
+        """ Reject the current state and close the dialog.
+
+        """
+        self.done(False)
+
+    def done(self, result):
+        """ Close the dialog with the given result.
+
+        Parameters
+        ----------
+        result : bool
+            True to accept the dialog or False to reject it.
+
+        """
+        if self.proxy_is_active:
+            self.proxy.done(result)
+
+    #--------------------------------------------------------------------------
+    # Observers
+    #--------------------------------------------------------------------------
+    @observe('title')
+    def _update_proxy(self, change):
+        """ An observer which updates the proxy when the data changes.
+
+        """
+        # The superclass implementation is sufficient.
+        super(ToolkitDialog, self)._update_proxy(change)
+
+    #--------------------------------------------------------------------------
+    # Utility Methods
+    #--------------------------------------------------------------------------
+    def _proxy_finished(self, result):
+        """ Called by the proxy object when the dialog is finished.
+
+        Parameters
+        ----------
+        result : bool
+            Wether or not the dialog was accepted.
+
+        """
+        self.result = result
+        self.finished(result)
+        if result:
+            self.accepted()
+        else:
+            self.rejected()
+        if self.callback:
+            self.callback(self)
+        if self.destroy_on_close:
+            deferred_call(self.destroy)
