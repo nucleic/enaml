a7c566bf38c83178014df9b4c1e529511cc0a9d4
Brett Patterson <brett.patterson0@gmail.com>
2013-07-03 14:23:43 -0500

    Added an IPythonPrompt widget

--- enaml/widgets/api.py
+++ enaml/widgets/api.py
@@ -26,6 +26,7 @@ from .form import Form
 from .group_box import GroupBox
 from .html import Html
 from .image_view import ImageView
+from .ipython_prompt import IPythonPrompt
 from .label import Label
 from .main_window import MainWindow
 from .mdi_area import MdiArea
---
+++ enaml/widgets/ipython_prompt.py
@@ -0,0 +1,59 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2013, Nucleic Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#------------------------------------------------------------------------------
+from atom.api import Typed, ForwardTyped, Dict, observe
+
+from enaml.core.declarative import d_
+
+from .control import Control, ProxyControl
+
+
+class ProxyIPythonPrompt(ProxyControl):
+    """ The abstract definition of a proxy IPythonWidget object.
+
+    """
+    #: A reference to the IPythonPrompt declaration.
+    declaration = ForwardTyped(lambda: IPythonPrompt)
+
+    def pull(self, identifier):
+        raise NotImplementedError
+
+    def set_context(self, context):
+        raise NotImplementedError
+
+
+class IPythonPrompt(Control):
+    """ An embedded IPython prompt
+
+    """
+    #: The context namespace for the prompt
+    context = d_(Dict())
+
+    #: A reference to the ProxyIPythonPrompt object.
+    proxy = Typed(ProxyIPythonPrompt)
+
+    #--------------------------------------------------------------------------
+    # Observers
+    #--------------------------------------------------------------------------
+    @observe(('context'))
+    def _update_proxy(self, change):
+        """ An observer which sends the state change to the proxy.
+
+        """
+        # The superclass implementation is sufficient.
+        super(IPythonPrompt, self)._update_proxy(change)
+
+    #--------------------------------------------------------------------------
+    # Public API
+    #--------------------------------------------------------------------------
+    def pull(self, identifier):
+        """ Attempt to pull an identifier from the IPython context
+
+        """
+        if self.proxy_is_active:
+            return self.proxy.pull(identifier)
+        return None
