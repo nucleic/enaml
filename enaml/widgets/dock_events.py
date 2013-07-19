322a345032ad9522bcd96c7d8aab3f116aba52fb
Chris Colbert <sccolbert@gmail.com>
2013-07-19 17:07:29 -0400

    use and int enum to distinguish dock item event types

--- enaml/widgets/dock_events.py
+++ enaml/widgets/dock_events.py
@@ -5,78 +5,60 @@
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #------------------------------------------------------------------------------
-from atom.api import Atom, Unicode
+from atom.api import Atom, IntEnum, Typed, Unicode
 
 
 class DockEvent(Atom):
-    """ A base class for defining events which occur in a dock area.
+    """ A sentinel base class for events which occur in a dock area.
 
     """
     pass
 
 
-class ItemDocked(DockEvent):
-    """ A dock event emitted when an item is docked in a dock area.
+class DockItemEvent(DockEvent):
+    """ A DockEvent for events which involve a single dock item.
 
     """
+    class Type(IntEnum):
+        """ An IntEnum which defines the dock item event types.
+
+        """
+        #: The dock item was docked in a dock area.
+        Docked = 0
+
+        #: The dock item was undocked from a dock area.
+        Undocked = 1
+
+        #: The dock item was extended from a dock bar.
+        Extended = 2
+
+        #: The dock item was retracted into a dock bar.
+        Retracted = 3
+
+        #: The dock item was shown on the screen.
+        Shown = 4
+
+        #: The dock item was hidden from the screen.
+        Hidden = 5
+
+        #: The dock item was closed.
+        Closed = 6
+
+        #: The dock item became the selected tab in a tab group.
+        TabSelected = 7
+
+    # Proxy out the enum values for simpler access.
+    Docked = Type.Docked
+    Undocked = Type.Undocked
+    Extended = Type.Extended
+    Retracted = Type.Retracted
+    Shown = Type.Shown
+    Hidden = Type.Hidden
+    Closed = Type.Closed
+    TabSelected = Type.TabSelected
+
+    #: The type of the dock item event.
+    type = Typed(Type)
+
     #: The name of the relevant dock item.
     name = Unicode()
-
-
-class ItemUndocked(DockEvent):
-    """ A dock event emitted when an item is undocked from an area.
-
-    """
-    #: The name of the relevant dock item.
-    name = Unicode()
-
-
-class ItemExtended(DockEvent):
-    """ A dock event emitted when an item is extended from a dock bar.
-
-    """
-    #: The name of the relevant dock item.
-    name = Unicode()
-
-
-class ItemRetracted(DockEvent):
-    """ A dock event emitted when an item is retracted into a dock bar.
-
-    """
-    #: The name of the relevant dock item.
-    name = Unicode()
-
-
-class ItemShown(DockEvent):
-    """ A dock event emitted when an item becomes visible on the screen.
-
-    """
-    #: The name of the relevant dock item.
-    name = Unicode()
-
-
-class ItemHidden(DockEvent):
-    """ A dock event emitted when an item is hidden from the screen.
-
-    """
-    #: The name of the relevant dock item.
-    name = Unicode()
-
-
-class ItemClosed(DockEvent):
-    """ A dock event emitted when an item is closed.
-
-    """
-    #: The name of the relevant dock item.
-    name = Unicode()
-
-
-class TabSelected(DockEvent):
-    """ A dock event emitted when a tab in tab group is selected.
-
-    """
-    #: The name of the currently selected dock item.
-    current = Unicode()
-
-    #: The name of the previously selected dock item.
-    previous = Unicode()
