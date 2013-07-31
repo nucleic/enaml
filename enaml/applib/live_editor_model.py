3e45dc6cd27cbe07e945f2983127c8a8f7bb8da1
Chris Colbert <sccolbert@gmail.com>
2013-07-31 15:04:36 -0400

    cleanup the live editor applib bits

--- enaml/applib/live_editor_model.py
+++ enaml/applib/live_editor_model.py
@@ -15,9 +15,21 @@ import enaml
 from enaml.core.object import Object
 from enaml.core.enaml_compiler import EnamlCompiler
 from enaml.core.parser import parse
+from enaml.widgets.widget import Widget
 
 
-def fake_linecache(text, filename):
+def _fake_linecache(text, filename):
+    """ Inject text into the linecache for traceback purposes.
+
+    Parameters
+    ----------
+    text : str
+        The text of the file.
+
+    filename : str
+        The name of the file.
+
+    """
     size = len(text)
     mtime = 1343290295L
     lines = text.splitlines()
@@ -26,19 +38,59 @@ def fake_linecache(text, filename):
 
 
 class LiveEditorModel(Atom):
-    """ The model which supports the live editor views.
+    """ A model which works in concert with the live editor panels.
+
+    This model manages the compiling and instantiation of the model
+    and view objects defined by the user.
+
+    The model has six inputs:
+
+        'model_text'
+            The full text of the Python module which defines the model.
+
+        'view_text'
+            The full text of the Enaml module which defines the view.
+
+        'model_item'
+            The name of the target model class in the model module.
+
+        'view_item'
+            The name of the target enamldef in the view module.
+
+        'model_filename'
+            An optional filename to associate with the model module.
+
+        'view_filename'
+            An optional filename to associate with the view module.
+
+    The model has three outputs:
+
+        'compiled_model'
+            The instance of the user defined model, or None if no model
+            could be created.
+
+        'compiled_view'
+            The instance of the user defined view, or None if no view
+            could be created.
+
+        'traceback'
+            A string holding the traceback for any compilation and
+            instantiation errors.
+
+    If the 'compiled_view' object has a 'model' attribute, then the
+    'compiled_model' object will be assigned to that attribute.
 
     """
     #: The current live model object bound to the main view.
-    output_model = Typed(Atom)
+    compiled_model = Typed(Atom)
 
     #: The current live view object to include in the main view.
-    output_view = Typed(Object)
+    compiled_view = Typed(Object)
 
-    #: The Python module text for the model module.
+    #: The Python module input text for the model module.
     model_text = Str()
 
-    #: The Enaml module text for the view module.
+    #: The Enaml module input text for the view module.
     view_text = Str()
 
     #: The string name of the Atom class to use as the model.
@@ -48,27 +100,37 @@ class LiveEditorModel(Atom):
     view_item = Str()
 
     #: An optional filename to use when compiling the python code.
-    py_filename = Str('main.py')
+    model_filename = Str('__live_model__.py')
 
     #: An optional filename to use when compiling the enaml code.
-    enaml_filename = Str('main.enaml')
+    view_filename = Str('__live_view__.enaml')
 
-    #: A string which holds the most recent trackback.
+    #: A string which holds the most recent tracekback.
     traceback = Str()
 
-    #: The module created from the model code.
+    #: The module created from the model text.
     _model_module = Typed(ModuleType)
 
-    #: The module created from the view code.
+    #: The module created from the view text.
     _view_module = Typed(ModuleType)
 
     #--------------------------------------------------------------------------
     # Post Validators
     #--------------------------------------------------------------------------
     def _post_validate_model_text(self, old, new):
+        """ Post validate the model text.
+
+        This validator replaces CRLF with LF characters.
+
+        """
         return new.replace('\r\n', '\n')
 
     def _post_validate_view_text(self, old, new):
+        """ Post validate the view text.
+
+        This validator replaces CRLF with LF characters.
+
+        """
         return new.replace('\r\n', '\n')
 
     #--------------------------------------------------------------------------
@@ -76,82 +138,100 @@ class LiveEditorModel(Atom):
     #--------------------------------------------------------------------------
     @observe(('model_text', 'model_item'))
     def _refresh_model_trigger(self, change):
+        """ An observer which triggers a compiled model refresh.
+
+        """
         self.refresh_model()
 
     @observe(('view_text', 'view_item'))
     def _refresh_view_trigger(self, change):
-        self.refresh_view()
+        """ An observer which triggers a compiled view refresh.
 
-    #--------------------------------------------------------------------------
-    # Private API
-    #--------------------------------------------------------------------------
-    def _build_model(self):
-        text = self.model_text
-        fake_linecache(text, self.py_filename)
-        if not text:
-            self.output_model = None
-            return
-        try:
-            code = compile(text, self.py_filename, 'exec')
-            module = ModuleType('__main__')
-            module.__file__ = self.py_filename
-            namespace = module.__dict__
-            exec code in namespace
-        except Exception:
-            self.traceback = traceback.format_exc()
-        else:
-            self.traceback = ''
-            self._model_module = module
-            model_class = namespace.get(self.model_item, lambda: None)
-            self.output_model = model_class()
+        """
+        self.refresh_view()
 
     #--------------------------------------------------------------------------
     # Public API
     #--------------------------------------------------------------------------
     def refresh_model(self):
-        self._build_model()
-        view = self.output_view
-        if view is not None and hasattr(view, 'model'):
-            view.model = self.output_model
+        """ Refresh the compiled model object.
 
-    def refresh_view(self):
-        text = self.view_text
-        fake_linecache(text, self.enaml_filename)
-        if not text:
-            self.output_view = None
-            return
+        This method will (re)compile the model for the given model text
+        and update the 'compiled_model' attribute. If a compiled view is
+        available and has a member named 'model', the model will be
+        applied to the view.
 
+        """
+        text = self.model_text
+        filename = self.model_filename
+        _fake_linecache(text, filename)
         try:
-            ast = parse(text, filename=self.enaml_filename)
-            code = EnamlCompiler.compile(ast, self.enaml_filename)
-            module = ModuleType('__main__')
-            module.__file__ = self.enaml_filename
-            namespace = module.__dict__
-            with enaml.imports():
+            if not text:
+                self.compiled_model = None
+                self._model_module = None
+            else:
+                code = compile(text, filename, 'exec')
+                module = ModuleType(filename.rsplit('.', 1)[0])
+                module.__file__ = filename
+                namespace = module.__dict__
                 exec code in namespace
+                model = namespace.get(self.model_item, lambda: None)()
+                self.compiled_model = model
+                self._model_module = module
+            self.relink_view()
         except Exception:
             self.traceback = traceback.format_exc()
-            return
+        else:
+            self.traceback = ''
 
-        self.traceback = ''
-        vc = namespace.get(self.view_item)
-        if isinstance(vc, type) and issubclass(vc, Object):
-            try:
-                if 'model' in vc.members():
-                    model = self.output_model
-                    view = vc(model=model)
-                else:
-                    view = vc()
-                old_view = self.output_view
+    def refresh_view(self):
+        """ Refresh the compiled view object.
+
+        This method will (re)compile the view for the given view text
+        and update the 'compiled_view' attribute. If a compiled model
+        is available and the view has a member named 'model', the model
+        will be applied to the view.
+
+        """
+        text = self.view_text
+        filename = self.view_filename
+        _fake_linecache(text, filename)
+        try:
+            if not text:
+                self.compiled_view = None
+                self._view_module = None
+            else:
+                ast = parse(text, filename=filename)
+                code = EnamlCompiler.compile(ast, filename)
+                module = ModuleType('__main__')
+                module.__file__ = filename
+                namespace = module.__dict__
+                with enaml.imports():
+                    exec code in namespace
+                view = namespace.get(self.view_item, lambda: None)()
+                if isinstance(view, Object) and 'model' in view.members():
+                    view.model = self.compiled_model
+                # trap any initialization errors and roll back the view
+                old = self.compiled_view
                 try:
-                    self.output_view = view
+                    self.compiled_view = view
                 except Exception:
-                    self.output_view = old_view
-                    if hasattr(old_view, 'show'):
-                        old_view.show()
+                    self.compiled_view = old
+                    if isinstance(old, Widget):
+                        old.show()
                     raise
                 self._view_module = module
-                if old_view is not None:
-                    old_view.destroy()
-            except Exception:
-                self.traceback = traceback.format_exc()
+                if old is not None and not old.is_destroyed:
+                    old.destroy()
+        except Exception:
+            self.traceback = traceback.format_exc()
+        else:
+            self.traceback = ''
+
+    def relink_view(self):
+        """ Relink the compiled view with the compiled model.
+
+        """
+        view = self.compiled_view
+        if view is not None and 'model' in view.members():
+            view.model = self.compiled_model
