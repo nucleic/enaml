7d050bea4b1febf7e81fbdf5dd698dcc85cf1bf2
Chris Colbert <sccolbert@gmail.com>
2013-08-23 12:19:41 -0400

    update looper for new compiler version
    The tree building process changed slightly with the new compiler. This
    brings Looper in line with the new version.

--- enaml/core/looper.py
+++ enaml/core/looper.py
@@ -14,17 +14,6 @@ from .conditional import Conditional
 from .declarative import Declarative, d_
 
 
-def populate_looper(looper, node, f_locals):
-    """ The populate function for the Looper class.
-
-    """
-    if node.scope_member:
-        node.scope_member.set_slot(looper, f_locals)
-    if node.identifier:
-        f_locals[node.identifier] = looper
-    looper._cdata.append((node, f_locals))
-
-
 class Looper(Declarative):
     """ A declarative object that repeats its children over an iterable.
 
@@ -56,20 +45,10 @@ class Looper(Declarative):
     #: Private data storage which maps the user iterable data to the
     #: list of items created for that iteration. This allows the looper
     #: to only create and destroy the items which have changed.
-    _idata = Typed(sortedmap, ())
+    _iter_data = Typed(sortedmap, ())
 
-    #: Private data storage for the looper.
-    _cdata = List()
-
-    @classmethod
-    def populator_func(cls):
-        """ Get the populator function for the Looper class.
-
-        This returns a populator function which intercepts the creation
-        of the child items and defers it until the initialization pass.
-
-        """
-        return populate_looper
+    #: Private data storage for the node data for the looper.
+    _node_data = List()
 
     #--------------------------------------------------------------------------
     # Lifetime API
@@ -101,7 +80,6 @@ class Looper(Declarative):
         parent = self.parent
         destroy_items = parent is not None and not parent.is_destroyed
         super(Looper, self).destroy()
-        parent = self.parent
         if destroy_items:
             for iteration in self.items:
                 for item in iteration:
@@ -109,8 +87,33 @@ class Looper(Declarative):
                         item.destroy()
         del self.iterable
         del self.items
-        del self._idata
-        del self._cdata
+        del self._iter_data
+        del self._node_data
+
+    def add_subtree(self, node, f_locals):
+        """ Add a node subtree to this looper.
+
+        This method changes the default behavior provided by the parent
+        class. It stores the node object and the locals mapping until
+        the object is initialized, at which point it creates the subtree
+        by repeating the node items according to the provided iterable.
+
+        Parameters
+        ----------
+        node : ConstructNode
+            A construct node containing the information required to
+            instantiate the children.
+
+        f_locals : mapping or None
+            A mapping object for the current local scope or None if
+            the block does not require a local scope.
+
+        """
+        if f_locals is not None:
+            if node.identifier:
+                f_locals[node.identifier] = self
+            self._d_storage[node.scope_key] = f_locals
+        self._node_data.append((node, f_locals))
 
     #--------------------------------------------------------------------------
     # Private API
@@ -122,7 +125,7 @@ class Looper(Declarative):
         items will be refreshed.
 
         """
-        if self.is_initialized:
+        if change['type'] == 'update' and self.is_initialized:
             self._refresh_items()
 
     def _refresh_items(self):
@@ -133,42 +136,47 @@ class Looper(Declarative):
 
         """
         old_items = self.items[:]
-        old_idata = self._idata
+        old_iter_data = self._iter_data
         iterable = self.iterable
-        cdata = self._cdata
-        new_idata = sortedmap()
+        node_data = self._node_data
+        new_iter_data = sortedmap()
         new_items = []
 
-        if iterable and len(cdata) > 0:
+        if iterable and len(node_data) > 0:
             for loop_index, loop_item in enumerate(iterable):
-                iteration = old_idata.get(loop_item)
+                iteration = old_iter_data.get(loop_item)
                 if iteration is not None:
-                    new_idata[loop_item] = iteration
+                    new_iter_data[loop_item] = iteration
                     new_items.append(iteration)
                     old_items.remove(iteration)
                     continue
                 iteration = []
-                new_idata[loop_item] = iteration
+                new_iter_data[loop_item] = iteration
                 new_items.append(iteration)
-                for node, f_locals in cdata:
-                    new_locals = f_locals.copy()
-                    new_locals['loop_index'] = loop_index
-                    new_locals['loop_item'] = loop_item
-                    for child_node in node.child_defs:
-                        child = child_node.typeclass()
-                        child_node.populate(child, child_node, new_locals)
+                for node, f_locals in node_data:
+                    if f_locals is not None:
+                        f_locals = f_locals.copy()
+                    else:
+                        f_locals = sortedmap()
+                    f_locals['loop_index'] = loop_index
+                    f_locals['loop_item'] = loop_item
+                    for child_node in node.children:
+                        child = child_node.klass()
+                        child.add_subtree(child_node, f_locals)
                         iteration.append(child)
 
         for iteration in old_items:
             for old in iteration:
                 if not old.is_destroyed:
                     old.destroy()
+
         if len(new_items) > 0:
             expanded = []
             _recursive_expand(sum(new_items, []), expanded)
             self.parent.insert_children(self, expanded)
+
         self.items = new_items
-        self._idata = new_idata
+        self._iter_data = new_iter_data
 
 
 def _recursive_expand(items, expanded):
