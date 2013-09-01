d0c6c4433a2107bf81b8f68cabcba3300645d4fd
Chris Colbert <sccolbert@gmail.com>
2013-08-31 23:20:00 -0400

    WIP commit of the template compiler
    This still needs a thorough review of the handling of local variables
    and static assignment expressions.

--- enaml/core/template_compiler.py
+++ enaml/core/template_compiler.py
@@ -5,6 +5,8 @@
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #------------------------------------------------------------------------------
+from atom.api import Typed
+
 from . import byteplay as bp
 from . import enaml_ast
 from .block_compiler import BlockCompiler
@@ -18,6 +20,9 @@ class TemplateCompiler(BlockCompiler):
     classmethod.
 
     """
+    #: The set of parameter and local variable names.
+    local_vars = Typed(set, ())
+
     @classmethod
     def compile(cls, node, filename):
         """ Compile a Template node into a code object.
@@ -57,34 +62,46 @@ class TemplateCompiler(BlockCompiler):
     #--------------------------------------------------------------------------
     # Utilities
     #--------------------------------------------------------------------------
-    def extract_template_locals(self, node):
-        """ Get whether or not a Template has local scope variables.
+    def load_name(self, name):
+        """ Load a name onto the TOS.
 
-        A template will have local scope variables if it has parameters,
-        const expressions, or child identifiers.
+        This is a reimplemented method which will load the name from
+        the fast locals if present, otherwise loading from globals.
+
+        """
+        if name in self.local_vars:
+            op = bp.LOAD_FAST
+        else:
+            op = bp.LOAD_GLOBAL
+        self.code_ops.append((op, name))
+
+    def collect_local_vars(self, node):
+        """ Collect the compile-time local variable names for the node.
 
         Parameters
         ----------
         node : Template
-            The template node of interest.
+            The enaml ast template node of interest.
 
         Returns
         -------
-        result : bool
-            True if the node has locals, False otherwise.
+        result : list
+            The list of local variable names found in the block.
 
         """
+        local_vars = []
         params = node.parameters
         for param in params.positional:
-            self.local_names.add(param.name)
+            local_vars.append(param.name)
         for param in params.keywords:
-            self.local_names.add(param.name)
+            local_vars.append(param.name)
         if params.starparam:
-            self.local_names.add(params.starparam)
+            local_vars.append(params.starparam)
         # The parser enforces that the StorageExpr is 'static'
-        for b in node.body:
-            if isinstance(b, enaml_ast.StorageExpr):
-                self.local_names.add(b.name)
+        for item in node.body:
+            if isinstance(item, enaml_ast.StorageExpr):
+                local_vars.append(item.name)
+        return local_vars
 
     #--------------------------------------------------------------------------
     # Visitors
@@ -93,11 +110,11 @@ class TemplateCompiler(BlockCompiler):
         """ The compiler visitor for a Template node.
 
         """
-        #self.extract_template_locals(node)
-        #if len(self.local_names) > 0:
-        #    self.has_locals = True
-        #elif self.has_identifiers(node):
-        #    self.has_locals = True
+        # Extract the local variables and check for other local scope.
+        self.local_vars.update(self.collect_local_vars(node))
+        self.has_locals = bool(self.local_vars)
+        if not self.has_locals:
+            self.has_locals = self.has_identifiers(node)
 
         # Claim the variable for the template node
         node_var = self.var_pool.new()
@@ -131,3 +148,56 @@ class TemplateCompiler(BlockCompiler):
 
         # Release the held variables
         self.var_pool.release(node_var)
+
+    def visit_StorageExpr(self, node):
+        """ The compiler visitor for a StorageExpr node.
+
+        This reimplements the base class visitor to specialize on
+        the template static expressions.
+
+        """
+        # XXX verify and document me!!!
+
+        # The parser has ensured that the storage is static. The code
+        # object reprenting the RHS of the expression must be rewritten
+        # to accept local variables where necessary.
+        self.visit(node.expr.value)
+        code = self.code_stack.pop()
+        bp_code = bp.Code.from_code(code).code
+
+        arg_names = []
+        stored_names = set()
+        local_vars = self.local_vars
+        for idx, (op, op_arg) in enumerate(bp_code):
+            if op == bp.STORE_NAME:
+                stored_names.add(op_arg)
+                bp_code[idx] = (bp.STORE_FAST, op_arg)
+        for idx, (op, op_arg) in enumerate(bp_code):
+            if op == bp.LOAD_NAME:
+                if op_arg in local_vars:
+                    op = bp.LOAD_FAST
+                    arg_names.append(op_arg)
+                elif op_arg in stored_names:
+                    op = bp.LOAD_FAST
+                else:
+                    op = bp.LOAD_GLOBAL
+                bp_code[idx] = (op, op_arg)
+            elif op == bp.DELETE_NAME:
+                bp_code[idx] = (bp.DELETE_FAST, op_arg)
+
+        code = bp.Code(
+            bp_code, [], arg_names, False, False, True, node.name,
+            self.filename, node.lineno, None
+        )
+
+        self.set_lineno(node.lineno)
+        self.code_ops.extend([
+            (bp.LOAD_CONST, code),  # code
+            (bp.MAKE_FUNCTION, 0),  # function
+        ])
+        for name in arg_names:
+            self.code_ops.append((bp.LOAD_FAST, name))
+        self.code_ops.extend([
+            (bp.CALL_FUNCTION, len(arg_names)),
+            (bp.STORE_FAST, node.name)
+        ])
