b351f316cc92d9518d48743893b994ca6182e3a4
Chris Colbert <sccolbert@gmail.com>
2013-09-12 11:32:18 -0400

    remove parser alias validation
    the validation is now done by the compiler

--- enaml/core/parser.py
+++ enaml/core/parser.py
@@ -329,57 +329,22 @@ def p_enaml_module_body2(p):
     p[0] = [p[1]]
 
 
-def _validate_enamldef_aliases(enamldef, lexer):
-    type_0 = enaml_ast.AliasExpr
-    func = lambda item: isinstance(item, type_0)
-    aliases = filter(func, enamldef.body)
-    if not aliases:
-        return
-    type_1 = (enaml_ast.EnamlDef, enaml_ast.ChildDef)
-    type_2 = enaml_ast.TemplateInst
-    obj_ids = set()
-    stack = [enamldef]
-    while stack:
-        node = stack.pop()
-        if isinstance(node, type_1):
-            if node.identifier:
-                obj_ids.add(node.identifier)
-            stack.extend(node.body)
-        elif isinstance(node, type_2) and node.identifiers:
-            obj_ids.update(node.identifiers.names)
-    alias_names = set()
-    for alias in aliases:
-        if alias.target not in obj_ids:
-            msg = "%s is not a valid alias target in this scope"
-            syntax_error(msg % alias.target, FakeToken(lexer, alias.lineno))
-        if alias.name in alias_names:
-            msg = "redefinition of alias %s"
-            syntax_error(msg % alias.name, FakeToken(lexer, alias.lineno))
-        alias_names.add(alias.name)
-
-
 def p_enaml_module_item1(p):
     ''' enaml_module_item : stmt
+                          | enamldef
                           | template '''
     p[0] = p[1]
 
 
 def p_enaml_module_item2(p):
-    ''' enaml_module_item : enamldef '''
-    _validate_enamldef_aliases(p[1], p.lexer.lexer)
-    p[0] = p[1]
-
-
-def p_enaml_module_item3(p):
     ''' enaml_module_item : decorators enamldef '''
-    enamldef = p[2]
-    _validate_enamldef_aliases(enamldef, p.lexer.lexer)
     decorators = []
     for decnode in p[1]:
         expr = ast.Expression()
         expr.body = decnode
         python = enaml_ast.PythonExpression(ast=expr, lineno=decnode.lineno)
         decorators.append(python)
+    enamldef = p[2]
     enamldef.decorators = decorators
     p[0] = enamldef
 
