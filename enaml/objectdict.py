b4ac98b0c1272cf980204b9ad07610ebccd2ab22
Chris Colbert <sccolbert@gmail.com>
2013-10-10 18:13:53 -0400

    add a dict subclass which adds attr access

---
+++ enaml/objectdict.py
@@ -0,0 +1,16 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2013, Nucleic Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#------------------------------------------------------------------------------
+class ObjectDict(dict):
+    """ A dict subclass which allows access by named attribute.
+
+    """
+    def __getattr__(self, attr):
+        if attr in self:
+            return self[attr]
+        msg = "'%s' object has no attribute '%s'"
+        raise AttributeError(msg % (type(self).__name__, attr))
