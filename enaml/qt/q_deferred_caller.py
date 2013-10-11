dab705b0d12925806b0bfde186161f31903294cb
Chris Colbert <sccolbert@gmail.com>
2013-10-11 10:18:01 -0400

    few more minor tweaks to deferred call functions

--- enaml/qt/q_deferred_caller.py
+++ enaml/qt/q_deferred_caller.py
@@ -24,15 +24,15 @@ class DeferredCallEvent(QEvent):
         self.kwargs = kwargs
 
 
-class QDeferredCaller(QObject):
+class DeferredCaller(QObject):
     """ A QObject subclass which handles deferred call events.
 
     """
     def __init__(self):
-        """ Initialize a QDeferredCaller.
+        """ Initialize a DeferredCaller.
 
         """
-        super(QDeferredCaller, self).__init__()
+        super(DeferredCaller, self).__init__()
         self.moveToThread(QApplication.instance().thread())
 
     def customEvent(self, event):
@@ -42,46 +42,29 @@ class QDeferredCaller(QObject):
         if event.type() == DeferredCallEvent.Type:
             event.callback(*event.args, **event.kwargs)
 
-    def deferredCall(self, callback, *args, **kwargs):
-        """ Execute the callback on the main gui thread.
-
-        Parameters
-        ----------
-        callback : callable
-            The callable object to execute on the main thread.
-
-        *args, **kwargs
-            Any additional positional and keyword arguments to pass to
-            the callback.
-
-        """
-        event = DeferredCallEvent(callback, args, kwargs)
-        QApplication.postEvent(self, event)
-
 
 #: A globally available caller instance. This will be created on demand
 #: by the globally available caller functions.
-_caller = None
+__caller = None
 
 
 def deferredCall(callback, *args, **kwargs):
     """ Execute the callback on the main gui thread.
 
-    This is a convenience wrapper around QDeferredCaller.deferredCall.
     This should only be called after the QApplication is created.
 
     """
-    global _caller
-    c = _caller
-    if c is None:
-        c = _caller = QDeferredCaller()
-    c.deferredCall(callback, *args, **kwargs)
+    global __caller
+    caller = __caller
+    if caller is None:
+        caller = __caller = DeferredCaller()
+    event = DeferredCallEvent(callback, args, kwargs)
+    QApplication.postEvent(caller, event)
 
 
 def timedCall(ms, callback, *args, **kwargs):
     """ Execute a callback on a timer in the main gui thread.
 
-    This is a convenience wrapper around QDeferredCaller.timedCall.
     This should only be called after the QApplication is created.
 
     """
