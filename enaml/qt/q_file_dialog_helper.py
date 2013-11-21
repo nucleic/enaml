771890ea7a441806ae07867e68b587a4bbeca75c
Chris Colbert <sccolbert@gmail.com>
2013-11-20 19:20:25 -0500

    minor cleanup

--- enaml/qt/q_file_dialog_helper.py
+++ enaml/qt/q_file_dialog_helper.py
@@ -8,6 +8,7 @@
 from . import QT_API
 from .QtGui import QFileDialog
 
+
 # A mapping from the Enaml dialog modes to the name of the static method
 # on QFileDialog which will launch the appropriate native dialog.
 if QT_API == 'pyqt':
@@ -29,8 +30,13 @@ else:
 def get_file_dialog_exec_func(mode):
     """ Get the appropriate static method for exec'ing a QFileDialog.
 
+    Parameters
+    ----------
+    mode : str
+        The target dialog mode. Must be one of: 'open_file',
+        'open_files', 'save_file', or 'directory'.
+
     """
     if mode not in _STATIC_METHOD_NAMES:
-        raise ValueError("Unknown file dialog mode: '{0}'".format(mode))
-
+        raise ValueError("Unknown file dialog mode: '%s'" % mode)
     return getattr(QFileDialog, _STATIC_METHOD_NAMES[mode])
