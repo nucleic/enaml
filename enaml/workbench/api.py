2a692ff474c69721b75d6baffb3eadfaf9a92b99
Chris Colbert <sccolbert@gmail.com>
2014-01-27 18:10:14 -0500

    add useful bits to the api file

--- enaml/workbench/api.py
+++ enaml/workbench/api.py
@@ -5,7 +5,11 @@
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #------------------------------------------------------------------------------
+from .extension import Extension
 from .extension_object import ExtensionObject
+from .extension_point import ExtensionPoint
 from .extension_registry import RegistryEventListener
 from .plugin import Plugin
+from .plugin_manifest import PluginManifest
 from .workbench import Workbench
+from .validation import validate
