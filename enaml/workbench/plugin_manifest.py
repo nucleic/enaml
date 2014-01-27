bc92f1e398008d287481a645fc7fc8843e70465b
Chris Colbert <sccolbert@gmail.com>
2014-01-27 11:24:47 -0500

    cleanup schema validation

--- enaml/workbench/plugin_manifest.py
+++ enaml/workbench/plugin_manifest.py
@@ -7,12 +7,17 @@
 #------------------------------------------------------------------------------
 import json
 import os
-import warnings
 
 from atom.api import Atom, List, Typed
 
 from .extension import Extension
 from .extension_point import ExtensionPoint
+from .validation import validate
+
+
+SCHEMA_PATH = os.path.abspath(__file__)
+SCHEMA_PATH = os.path.dirname(SCHEMA_PATH)
+SCHEMA_PATH = os.path.join(SCHEMA_PATH, 'schema.json')
 
 
 class PluginManifest(Atom):
@@ -59,91 +64,7 @@ class PluginManifest(Atom):
         return self.data.get(u'description', u'')
 
 
-class ManifestValidator(object):
-    """ A helper class for validating plugin manifest JSON data.
-
-    This class should not be used directly by user code.
-
-    """
-    #: A reference to the singleton validator instance.
-    __instance__ = None
-
-    @staticmethod
-    def load_validator():
-        """ Load the schema validator function.
-
-        Returns
-        -------
-        result : callable
-            A callable which validates the schema. It will have a
-            call signature which matches jsonschema.validate.
-
-        """
-        try:
-            import jsonschema
-        except:
-            msg = "The 'jsonschema' package is not installed. "
-            msg += "Plugin manifest validation will be skipped."
-            warnings.warn(msg)
-            validator = lambda i, s, c=None, *a, **k: None
-        else:
-            validator = jsonschema.validate
-        return validator
-
-    @staticmethod
-    def load_schema():
-        """ Load the schema data from the schema file.
-
-        Returns
-        -------
-        result : object
-            The schema data object.
-
-        """
-        abspath = os.path.abspath(__file__)
-        dirname = os.path.dirname(abspath)
-        path = os.path.join(dirname, 'schema.json')
-        with open(path) as f:
-            data = f.read()
-        return json.loads(data)
-
-    @classmethod
-    def instance(cls):
-        """ Get a reference to a singleton validator instance.
-
-        Returns
-        -------
-        result : ManifestValidator
-            A reference to a singleton validator instance.
-
-        """
-        if cls.__instance__ is None:
-            cls.__instance__ = cls()
-        return cls.__instance__
-
-    def __init__(self):
-        """ Initialize a ManifestValidator.
-
-        """
-        self._validator = self.load_validator()
-        self._schema = self.load_schema()
-
-    def __call__(self, instance):
-        """ Validate the JSON data instance.
-
-        This method will validate the instance against the plugin
-        manifest schema.
-
-        Parameters
-        ----------
-        instance : object
-            The root object loaded from the JSON file.
-
-        """
-        self._validator(instance, self._schema)
-
-
-def create_manifest(data, validate):
+def create_manifest(data):
     """ Create a plugin manifest from JSON data.
 
     This function should not be used directly by user code.
@@ -154,10 +75,6 @@ def create_manifest(data, validate):
         The json manifest data. If this is a str, it must be encoded
         in UTF-8 or plain ASCII.
 
-    validate : bool
-        Whether to validate the json against the manifest schema. This
-        requires the 'jsonschema' package to be installed.
-
     Returns
     -------
     result : PluginManifest
@@ -165,8 +82,7 @@ def create_manifest(data, validate):
 
     """
     root = json.loads(data)
-    if validate:
-        ManifestValidator.instance()(root)
+    validate(root, SCHEMA_PATH)
     manifest = PluginManifest(data=root)
     plugin_id = manifest.id
     for pt in root.get(u'extension_points', ()):
