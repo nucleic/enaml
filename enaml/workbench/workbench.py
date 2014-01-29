e4c411472dd6af6221a47b3d6fd317a249ea52d5
Chris Colbert <sccolbert@gmail.com>
2014-01-29 14:55:47 -0500

    update the workbench to support change notification

--- enaml/workbench/workbench.py
+++ enaml/workbench/workbench.py
@@ -5,16 +5,17 @@
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #------------------------------------------------------------------------------
+from collections import defaultdict
 import json
 import os
 import traceback
 import warnings
 
-from atom.api import Atom, Typed
+from atom.api import Atom, Event, Typed
 
 from .extension import Extension
 from .extension_object import ExtensionObject
-from .extension_point import ExtensionPoint
+from .extension_point import ExtensionPoint, ExtensionPointEvent
 from .plugin import Plugin
 from .plugin_manifest import PluginManifest
 from .validation import validate
@@ -22,7 +23,100 @@ from .validation import validate
 
 SCHEMA_PATH = os.path.abspath(__file__)
 SCHEMA_PATH = os.path.dirname(SCHEMA_PATH)
-SCHEMA_PATH = os.path.join(SCHEMA_PATH, 'schema.json')
+SCHEMA_PATH = os.path.join(SCHEMA_PATH, 'plugin_schema.json')
+
+
+def _import_object(path):
+    """ Import an object from a dot separated path.
+
+    Parameters
+    ----------
+    path : unicode
+        A dot separated path of the form 'pkg.module.item' which
+        represents the import path to the object.
+
+    Returns
+    -------
+    result : object
+        The item pointed to by the path. An import error will be
+        raised if the item cannot be imported.
+
+    """
+    if u'.' not in path:
+        return __import__(path)
+    path, item = path.rsplit(u'.', 1)
+    mod = __import__(path, {}, {}, [item])
+    try:
+        result = getattr(mod, item)
+    except AttributeError:
+        raise ImportError(u'cannot import name %s' % item)
+    return result
+
+
+def _create_plugin(manifest):
+    """ Create a plugin instance for the given manifest.
+
+    Parameters
+    ----------
+    manifest : PluginManifest
+        The manifest which describes the plugin to create.
+
+    Returns
+    -------
+    result : Plugin or None
+        A new Plugin instance or None if one could not be created.
+
+    """
+    path = manifest.cls
+    if not path:
+        return Plugin()
+
+    try:
+        plugin_cls = _import_object(path)
+    except ImportError:
+        msg = "failed to import plugin class '%s':\n%s"
+        warnings.warn(msg % (path, traceback.format_exc()))
+        return None
+
+    plugin = plugin_cls()
+    if not isinstance(plugin, Plugin):
+        msg = "plugin class '%s' created non-Plugin type '%s'"
+        warnings.warn(msg % (path, type(plugin).__name__))
+        return None
+
+    return plugin
+
+
+def _create_extension_object(extension):
+    """ Create the implementation object for a given extension.
+
+    Parameters
+    ----------
+    extension : Extension
+        The extension which contains the path to the object class.
+
+    Returns
+    -------
+    result : ExtensionObject or None
+        The newly created extension object, or None if one could
+        not be created.
+
+    """
+    path = extension.cls
+    try:
+        extension_class = _import_object(path)
+    except ImportError:
+        msg = "failed to load extension class '%s':\n%s"
+        warnings.warn(msg % (path, traceback.format_exc()))
+        return None
+
+    obj = extension_class()
+    if not isinstance(obj, ExtensionObject):
+        msg = "extension class '%s' created non-ExtensionObject type '%s'"
+        warnings.warn(msg % (path, type(obj).__name__))
+        return None
+
+    return obj
 
 
 class Workbench(Atom):
@@ -33,16 +127,35 @@ class Workbench(Atom):
     by a subclass, such as the enaml.studio.Studio class.
 
     """
+    #: An event fired when a plugin is added to the workbench. The
+    #: payload will be the plugin id.
+    plugin_added = Event(unicode)
+
+    #: An event fired when a plugin is removed from the workbench. The
+    #: payload will be the plugin id.
+    plugin_removed = Event(unicode)
+
+    #: An event fired when an extension point is added to the
+    #: workbench. The payload will be the fully qualified id of the
+    #: extension point.
+    extension_point_added = Event(unicode)
+
+    #: An event fired when an extension point is removed from the
+    #: workbench. The payload will be the fully qualified id of the
+    #: extension point.
+    extension_point_removed = Event(unicode)
+
     def register(self, data):
         """ Register a plugin with the workbench.
 
         Parameters
         ----------
         data : str or unicode
-            The JSON plugin manifest data. If this is a str, it must be
-            encoded in UTF-8 or plain ASCII.
+            The JSON plugin manifest data. If this is a str, it must
+            be encoded in UTF-8 or plain ASCII.
 
         """
+        # TODO unify validation
         root = json.loads(data)
         validate(root, SCHEMA_PATH)
 
@@ -64,6 +177,8 @@ class Workbench(Atom):
         self._manifests[plugin_id] = manifest
         self._add_extension_points(points)
         self._add_extensions(extensions)
+
+        self.plugin_added(plugin_id)
 
     def unregister(self, plugin_id):
         """ Remove a plugin from the workbench.
@@ -93,6 +208,8 @@ class Workbench(Atom):
         self._remove_extension_points(manifest.extension_points)
         del self._manifests[plugin_id]
 
+        self.plugin_removed(plugin_id)
+
     def get_manifest(self, plugin_id):
         """ Get the plugin manifest for a given plugin id.
 
@@ -104,8 +221,8 @@ class Workbench(Atom):
         Returns
         -------
         result : PluginManifest or None
-            The manifest for the plugin of interest, or None if it does
-            not exist.
+            The manifest for the plugin of interest, or None if it
+            does not exist.
 
         """
         return self._manifests.get(plugin_id)
@@ -134,14 +251,14 @@ class Workbench(Atom):
 
         manifest = self._manifests.get(plugin_id)
         if manifest is None:
-            msg = "plugin manifest for plugin '%s' is not registered"
+            msg = "plugin '%s' is not registered"
             warnings.warn(msg % plugin_id)
             return None
 
         if not force_create:
             return None
 
-        plugin = self._create_plugin(manifest)
+        plugin = _create_plugin(manifest)
         self._plugins[plugin_id] = plugin
         if plugin is None:
             return None
@@ -170,7 +287,7 @@ class Workbench(Atom):
 
         """
         self.get_plugin(extension.plugin_id)  # ensure plugin is activated
-        obj = self._create_extension_object(extension)
+        obj = _create_extension_object(extension)
         if obj is None:
             return None
 
@@ -215,38 +332,14 @@ class Workbench(Atom):
     #: A mapping of plugin id to Plugin instance.
     _plugins = Typed(dict, ())
 
-    #: A mapping of extension point id to extension point.
+    #: A mapping of extension point id to ExtensionPoint.
     _extension_points = Typed(dict, ())
 
-    #: A mapping of extension point id to list of extensions.
-    _contributions = Typed(dict, ())
+    #: A mapping of extension id to Extension.
+    _extensions = Typed(dict, ())
 
-    @staticmethod
-    def _import_object(path):
-        """ Import an object from a dot separated path.
-
-        Parameters
-        ----------
-        path : unicode
-            A dot separated path of the form 'pkg.module.item' which
-            represents the import path to the object.
-
-        Returns
-        -------
-        result : object
-            The item pointed to by the path. An import error will be
-            raised if the item cannot be imported.
-
-        """
-        if u'.' not in path:
-            return __import__(path)
-        path, item = path.rsplit(u'.', 1)
-        mod = __import__(path, {}, {}, [item])
-        try:
-            result = getattr(mod, item)
-        except AttributeError:
-            raise ImportError(u'cannot import name %s' % item)
-        return result
+    #: A mapping of extension point id to set of Extensions.
+    _contributions = Typed(defaultdict, (set,))
 
     def _add_extension_points(self, extension_points):
         """ Add extension points to the workbench.
@@ -276,8 +369,12 @@ class Workbench(Atom):
             warnings.warn(msg % point_id)
             return
 
-        # TODO add any registered extensions to the point.
         self._extension_points[point_id] = point
+        if point_id in self._contributions:
+            to_add = list(self._contributions[point_id])
+            self._update_extension_point(point_id, [], to_add)
+
+        self.extension_point_added(point_id)
 
     def _remove_extension_points(self, extension_points):
         """ Remove extension points from the workbench.
@@ -306,8 +403,12 @@ class Workbench(Atom):
             warnings.warn(msg % point_id)
             return
 
-        # TODO remove any extensions from the point.
         del self._extension_points[point_id]
+        if point_id in self._contributions:
+            to_remove = list(self._contributions.pop(point_id))
+            self._update_extension_point(point_id, to_remove, [])
+
+        self.extension_point_removed(point_id)
 
     def _add_extensions(self, extensions):
         """ Add extensions to the workbench.
@@ -318,24 +419,20 @@ class Workbench(Atom):
             The list of Extensions to add to the workbench.
 
         """
-        # TODO validate the extensions against the schema
-        # key = lambda ext: ext.point
-        # for point_id, extensioniter in groupby(extensions, key):
-        #     added = []
-        #     for extension in extensioniter:
-        #         ext_id = extension.qualified_id
-        #         if ext_id and ext_id in self._extension_ids:
-        #             msg = "The extension '%s' is already registered. "
-        #             msg += "The duplicate extension will be ignored."
-        #             warnings.warn(msg % ext_id)
-        #         else:
-        #             if ext_id:
-        #                 self._extension_ids.add(ext_id)
-        #             added.append(extension)
-        #     if added:
-        #         self._contributions[point_id].extend(added)
-        #         # method_name = 'extensions_added'
-        #         # self._invoke_listeners(point_id, method_name, added)
+        grouped = defaultdict(list)
+        for extension in extensions:
+            ext_id = extension.qualified_id
+            if ext_id in self._extensions:
+                msg = "The extension '%s' is already registered. "
+                msg += "The duplicate extension will be ignored."
+                warnings.warn(msg % ext_id)
+                continue
+            self._extensions[ext_id] = extension
+            grouped[extension.point].append(extension)
+
+        for point_id, exts in grouped.iteritems():
+            self._contributions[point_id].update(exts)
+            self._update_extension_point(point_id, [], exts)
 
     def _remove_extensions(self, extensions):
         """ Remove extensions from the workbench.
@@ -346,84 +443,64 @@ class Workbench(Atom):
             The list of the Extensions to remove from the workbench.
 
         """
-        # key = lambda ext: ext.point
-        # for point_id, extensioniter in groupby(extensions, key):
-        #     current = self._contributions.get(point_id, [])
-        #     removed = []
-        #     for extension in extensioniter:
-        #         ext_id = extension.qualified_id
-        #         try:
-        #             current.remove(extension)
-        #         except ValueError:
-        #             msg = "The extension '%s' is not registered."
-        #             warnings.warn(msg % ext_id)
-        #         else:
-        #             self._extension_ids.discard(ext_id)
-        #             removed.append(extension)
-        #     # if removed:
-        #     #     method_name = 'extensions_removed'
-        #     #     self._invoke_listeners(point_id, method_name, removed)
+        grouped = defaultdict(list)
+        for extension in extensions:
+            ext_id = extension.qualified_id
+            if ext_id not in self._extension:
+                msg = "The extension '%s' is not registered."
+                warnings.warn(msg % ext_id)
+                continue
+            del self._extensions[ext_id]
+            grouped[extension.point].append(extension)
 
-    def _create_plugin(self, manifest):
-        """ Create a plugin instance for the given manifest.
+        for point_id, exts in grouped.iteritems():
+            self._contributions[point_id].difference_update(exts)
+            self._update_extension_point(point_id, exts, [])
 
-        Parameters
-        ----------
-        manifest : PluginManifest
-            The manifest which describes the plugin to create.
+    def _update_extension_point(self, point_id, to_remove, to_add):
+        """ Update an extension point will delta extension objects.
 
-        Returns
-        -------
-        result : Plugin or None
-            A new Plugin instance or None if one could not be created.
+        This will update the extension point's extensions according to
+        the deltas and emit an update event on the point. Newly added
+        extensions will be validated against the extension point schema
+        if one is available.
 
-        """
-        path = manifest.cls
-        if not path:
-            return Plugin()
-
-        try:
-            plugin_cls = self._import_object(path)
-        except ImportError:
-            msg = "failed to import plugin class '%s':\n%s"
-            warnings.warn(msg % (path, traceback.format_exc()))
-            return None
-
-        plugin = plugin_cls()
-        if not isinstance(plugin, Plugin):
-            msg = "plugin '%s' created non-Plugin type '%s'"
-            warnings.warn(msg % (path, type(plugin).__name__))
-            return None
-
-        return plugin
-
-    def _create_extension_object(self, extension):
-        """ Create the implementation object for a given extension.
+        If the given extension point has not yet been added to the
+        workbench, this method is a no-op.
 
         Parameters
         ----------
-        extension : Extension
-            The extension which contains the path to the object class.
+        point_id : unicode
+            The qualified id of the extension point of interest.
 
-        Returns
-        -------
-        result : ExtensionObject or None
-            The newly created extension object, or None if one could
-            not be created.
+        to_remove : list
+            The list of Extension objects to remove from the point.
+
+        to_add : list
+            The list of Extension objects to add to the point.
 
         """
-        path = extension.cls
-        try:
-            extension_class = self._import_object(path)
-        except ImportError:
-            msg = "failed to load extension class '%s':\n%s"
-            warnings.warn(msg % (path, traceback.format_exc()))
-            return None
+        point = self._extension_points.get(point_id)
+        if point is None:
+            return
 
-        obj = extension_class()
-        if not isinstance(obj, ExtensionObject):
-            msg = "extension '%s' created non-ExtensionObject type '%s'"
-            warnings.warn(msg % (path, type(obj).__name__))
-            return None
+        extensions = set(point._extensions)
 
-        return obj
+        removed = []
+        for ext in to_remove:
+            if ext in extensions:
+                extensions.remove(ext)
+                removed.append(ext)
+
+        added = []
+        for ext in to_add:
+            if ext not in extensions:
+                # TODO validate the against the extension point schema
+                extensions.add(ext)
+                added.append(ext)
+
+        if removed or added:
+            key = lambda ext: ext.rank
+            point._extensions = sorted(extensions, key=key)
+            event = ExtensionPointEvent(removed=removed, added=added)
+            point.updated(event)
