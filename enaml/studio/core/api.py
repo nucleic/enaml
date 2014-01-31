2670c135a6404396ffc786bde66833af6ea7f3ab
S. Chris Colbert <sccolbert@gmail.com>
2014-01-30 22:02:40 -0500

    remove stale files

--- enaml/studio/core/api.py
+++
@@ -1,9 +0,0 @@
-#------------------------------------------------------------------------------
-# Copyright (c) 2013, Nucleic Development Team.
-#
-# Distributed under the terms of the Modified BSD License.
-#
-# The full license is in the file COPYING.txt, distributed with this software.
-#------------------------------------------------------------------------------
-from .file_loader import FileLoader
-from .url_loader import URLLoader
