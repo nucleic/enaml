7414c649111810329fa36a5c023ebe69e37319df
S. Chris Colbert <sccolbert@gmail.com>
2014-02-04 15:20:44 -0500

    add lifecycle methods to the Workspace class

--- enaml/workbench/ui/workspace.py
+++ enaml/workbench/ui/workspace.py
@@ -9,6 +9,7 @@ from atom.api import Typed, Unicode
 
 from enaml.core.declarative import Declarative, d_
 from enaml.widgets.container import Container
+from enaml.workbench.workbench import Workbench
 
 
 class Workspace(Declarative):
@@ -22,10 +23,28 @@ class Workspace(Declarative):
     #: destroyed automatically when the workspace is disposed.
     content = d_(Typed(Container))
 
-    def dispose(self):
-        """ Dispose of any resources allocated by the workspace.
+    #: The workbench object which owns the workspace. This will be
+    #: assigned when the ui plugin creates the workspace. It will
+    #: be available by the time the 'start' method is called.
+    workbench = Typed(Workbench)
+
+    def start(self):
+        """ Start the workspace.
+
+        This method is called when the UI plugin starts the workspace.
+        This can be used to load content or any other resource which
+        should exisit for the life of the workspace.
+
+        """
+        pass
+
+    def stop(self):
+        """ Stop the workspace.
 
         This method is called when the UI plugin closes the workspace.
+        This should be used to release any resources acquired during
+        the lifetime of the workspace. The content Container will be
+        destroyed automatically after this method returns.
 
         """
         pass
