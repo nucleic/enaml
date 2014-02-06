ead0b80b93c3c27de74cfc71f0d575b4a0e4a2f7
Chris Colbert <sccolbert@gmail.com>
2014-02-06 16:04:20 -0500

    update example

--- sample.py
+++
@@ -1,9 +0,0 @@
-from enaml.workbench.ui.api import UIWorkbench
-import enaml
-
-with enaml.imports():
-    from sample_plugin import SampleManifest
-
-workbench = UIWorkbench()
-workbench.register(SampleManifest())
-workbench.run()
