c809290e8102184a3f807814b6e162c69aa236e0
Chris Colbert <sccolbert@gmail.com>
2014-02-07 13:28:17 -0500

    fix validator logic in example

--- examples/tutorial/employee/phone_validator.py
+++ examples/tutorial/employee/phone_validator.py
@@ -21,18 +21,33 @@ class PhoneNumberValidator(Validator):
     proper = re.compile(r'\(([0-9]{3})\)\ ([0-9]{3})\-([0-9]{4})$')
 
     def validate(self, text):
-        match = self.proper.match(text) or self.dashes.match(text)
+        """ Validate the input text.
+
+        The text must be in of the form: (555) 555-5555 in order to
+        pass the standard validation. The fixup method will convert
+        some alternative forms into a correct format.
+
+        """
+        return bool(self.proper.match(text))
+
+    def fixup(self, text):
+        """ Attempt to convert the given text into the proper format.
+
+        This method is called by the backend when the current text is
+        not valid, but can maybe be *made* to be valid by this method.
+        The returned text is re-validated to test for viability.
+
+        """
+        match = self.dashes.match(text)
         if match:
             area = match.group(1)
             prefix = match.group(2)
             suffix = match.group(3)
-            formatted_text = '(%s) %s-%s' % (area, prefix, suffix)
-            return formatted_text, True
+            return u'(%s) %s-%s' % (area, prefix, suffix)
         match = self.all_digits.match(text)
         if match:
             area = text[:3]
             prefix = text[3:6]
             suffix = text[6:10]
-            formatted_text = '(%s) %s-%s' % (area, prefix, suffix)
-            return formatted_text, True
-        return text, False
+            return u'(%s) %s-%s' % (area, prefix, suffix)
+        return text
