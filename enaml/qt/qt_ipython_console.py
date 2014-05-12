bac91717234e76e3f5efb6c01e419b862343c07a
S. Chris Colbert <sccolbert@gmail.com>
2014-05-12 13:13:02 +0100

    update focus handling for the ipython console

--- enaml/qt/qt_ipython_console.py
+++ enaml/qt/qt_ipython_console.py
@@ -13,8 +13,8 @@ from IPython.qt.console.rich_ipython_widget import RichIPythonWidget
 from IPython.qt.inprocess import QtInProcessKernelManager
 
 from .QtGui import QFrame, QVBoxLayout
-from .QtCore import Qt
 
+from . import focus_registry
 from .qt_control import QtControl
 
 
@@ -22,7 +22,9 @@ class QtIPythonConsole(QtControl, ProxyIPythonConsole):
     """ A Qt4 implementation of an Enaml IPythonConsole.
 
     """
-    #: The wrapper widget created by the proxy.
+    #: The wrapper widget created by the proxy. A wrapper is necessary
+    #: since the IPython widget overrides critical Qt API methods which
+    #: renders the widget incompatible with the ancestor hierarchy.
     widget = Typed(QFrame)
 
     #: The internal IPython console widget.
@@ -34,24 +36,16 @@ class QtIPythonConsole(QtControl, ProxyIPythonConsole):
         """
         self.widget = QFrame(self.parent_widget())
         self.ipy_widget = RichIPythonWidget()
+        assert self.page_control is not None  # always use paging
 
     def init_widget(self):
         """ Initialize the underlying widget.
 
         """
         super(QtIPythonConsole, self).init_widget()
-        kernel_manager = QtInProcessKernelManager()
-        kernel_manager.start_kernel()
-        kernel_client = kernel_manager.client()
-        kernel_client.start_channels()
-        ipy_widget = self.ipy_widget
-        ipy_widget.kernel_manager = kernel_manager
-        ipy_widget.kernel_client = kernel_client
-        control = ipy_widget._control
-        control._d_proxy = self
-        widget = self.widget
-        widget.setFocusPolicy(Qt.StrongFocus)
-        widget.setFocusProxy(control)
+        self.setup_kernel()
+        focus_registry.register(self.text_control, self)
+        focus_registry.register(self.page_control, self)
 
     def init_layout(self):
         """ Initialize the underlying widget layout.
@@ -68,8 +62,60 @@ class QtIPythonConsole(QtControl, ProxyIPythonConsole):
         """ Destroy the underlying widget.
 
         """
+        self.teardown_kernel()
+        focus_registry.unregister(self.text_control)
+        focus_registry.unregister(self.page_control)
+        super(QtIPythonConsole, self).destroy()
+
+    def setup_kernel(self):
+        """ Setup the kernel for the widget.
+
+        """
+        kernel_manager = QtInProcessKernelManager()
+        kernel_manager.start_kernel()
+        kernel_client = kernel_manager.client()
+        kernel_client.start_channels()
+        ipy_widget = self.ipy_widget
+        ipy_widget.kernel_manager = kernel_manager
+        ipy_widget.kernel_client = kernel_client
+
+    def teardown_kernel(self):
+        """ Teardown the kernel for the widget.
+
+        """
         ipy_widget = self.ipy_widget
         ipy_widget.kernel_client.stop_channels()
         ipy_widget.kernel_manager.shutdown_kernel()
-        ipy_widget._control._d_proxy = None
-        super(QtIPythonConsole, self).destroy()
+
+    @property
+    def text_control(self):
+        """ Return the text control for the IPython widget.
+
+        Returns
+        -------
+        result : QTextEdit
+            The text control for the IPython widget.
+
+        """
+        return self.ipy_widget._control
+
+    @property
+    def page_control(self):
+        """ Return the page control for the IPython widget.
+
+        Returns
+        -------
+        result : QTextEdit
+            The page control for the IPython widget.
+
+        """
+        return self.ipy_widget._page_control
+
+    def focus_target(self):
+        """ Returns the current focus target for the widget.
+
+        """
+        page = self.page_control
+        if page.isVisibleTo(self.widget):
+            return page
+        return self.text_control
