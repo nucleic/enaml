ff1dbca5217833eb2cee35c2ea124abd33db94d2
MatthieuDartiailh <m.dartiailh@gmail.com>
2016-05-24 21:20:25 +0200

    enaml : add a compat module

---
+++ enaml/compat.py
@@ -0,0 +1,10 @@
+#------------------------------------------------------------------------------
+# Copyright (c) 2013, Nucleic Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#------------------------------------------------------------------------------
+import sys
+
+IS_PY3 = sys.version_info >= (3,)
