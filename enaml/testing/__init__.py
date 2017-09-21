c99281181a219d750840eb6ea6eece4a7d020179
Matthieu Dartiailh <marul@laposte.net>
2017-09-21 17:26:06 +0200

    testing: update __init__ header
    [ci skip]

--- enaml/testing/__init__.py
+++ enaml/testing/__init__.py
@@ -1,2 +1,7 @@
-# -*- coding: utf-8 -*-
-
+#------------------------------------------------------------------------------
+# Copyright (c) 2013-2017, Nucleic Development Team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#------------------------------------------------------------------------------
