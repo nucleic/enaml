815cbd82ff6d09e4a1c221ebe425aa4518dbaed3
frmdstryr <frmdstryr@protonmail.com>
2018-07-15 19:22:50 -0400

    Add more interactive dock test

--- tests/test_dock_layout.py
+++ tests/test_dock_layout.py
@@ -6,6 +6,7 @@
 #
 # The full license is in the file COPYING.txt, distributed with this software.
 #------------------------------------------------------------------------------
+import os
 import pytest
 from utils import is_qt_available
 
@@ -68,4 +69,91 @@ def test_validation_dock_layout2(enaml_qtbot, enaml_sleep):
     with pytest.warns(DockLayoutWarning):
         win.area.layout = HSplitLayout('item1', 'item2', 'item3')
     assert globals() == glob
+    enaml_qtbot.wait(enaml_sleep)
+    
+    
+def test_dock_area_interactions(enaml_qtbot, enaml_sleep):
+    """Test interations with the dock area.
+
+    14013 lines
+
+    """
+    from enaml.qt.QtCore import Qt
+    with open(os.path.join('examples', 'widgets', 'dock_area.enaml')) as f:
+        source = f.read()
+    
+    win = compile_source(source, 'Main')()
+    win.show()
+    
+    wait_for_window_displayed(enaml_qtbot, win)
+    enaml_qtbot.wait(enaml_sleep)
+    _children = win.children[0].children
+    btn_save, btn_restore, btn_add, cmb_styles, cbx_evts, dock = _children
+        
+    # Enable dock events
+    enaml_qtbot.mouseClick(cbx_evts.proxy.widget, Qt.LeftButton)
+    
+    enaml_sleep = 1000
+    enaml_qtbot.wait(enaml_sleep)
+    
+    with pytest.warns(DockLayoutWarning):
+        # Change styles
+        cmb_styles.proxy.widget.setFocus()
+        enaml_qtbot.keyClick(cmb_styles.proxy.widget, Qt.Key_Up)
+        
+        # Save layout
+        enaml_qtbot.mouseClick(btn_save.proxy.widget, Qt.LeftButton)
+        enaml_qtbot.wait(enaml_sleep)
+        
+        # Toggle closable
+        di = dock.find('item_2')
+        di.closable = False
+        enaml_qtbot.wait(enaml_sleep)
+        di.closable = True
+        
+        # Check alert
+        di.alert('info')
+        
+        # Maximize it
+        tb = di.proxy.widget.titleBarWidget()
+        enaml_qtbot.mouseClick(tb._max_button, Qt.LeftButton)
+        enaml_qtbot.wait(enaml_sleep)
+        
+        # Minimize it
+        enaml_qtbot.mouseClick(tb._restore_button, Qt.LeftButton)
+        enaml_qtbot.wait(enaml_sleep)
+    
+        # Pin it
+        enaml_qtbot.mouseClick(tb._pin_button, Qt.LeftButton)
+        enaml_qtbot.wait(enaml_sleep)
+        
+        # TODO: Open and close it using clicks
+        dock_area = dock.proxy.widget
+        for c in dock_area.dockBarContainers():
+            dock_bar = c[0]
+            dock_area.extendFromDockBar(dock_bar)
+            enaml_qtbot.wait(enaml_sleep)
+            dock_area.retractToDockBar(dock_bar)
+            enaml_qtbot.wait(enaml_sleep)
+        
+        # Unpin
+        enaml_qtbot.mouseClick(tb._pin_button, Qt.LeftButton)
+        enaml_qtbot.wait(enaml_sleep)
+        
+        # Add items
+        enaml_qtbot.mouseClick(btn_add.proxy.widget, Qt.LeftButton)
+        enaml_qtbot.wait(enaml_sleep)
+        
+        # Close all added items
+        for i in range(10, 15):
+            di = dock.find('item_%i'%i)
+            if di:
+                enaml_qtbot.mouseClick(
+                    di.proxy.widget.titleBarWidget()._close_button, 
+                    Qt.LeftButton)
+        
+        # Restore layout
+        enaml_qtbot.mouseClick(btn_restore.proxy.widget, Qt.LeftButton)
+        enaml_qtbot.wait(enaml_sleep)
+    
     enaml_qtbot.wait(enaml_sleep)
