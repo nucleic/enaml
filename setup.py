f453d133dc97f08ff29fd017ccc4d31e220e3818
MatthieuDartiailh <marul@laposte.net>
2019-04-22 21:27:13 -0400

    setup: downgrade kiwi version
    We do not need the newest since we do not use dump and the one bug that was fixed does not affect us.

--- setup.py
+++ setup.py
@@ -166,7 +166,7 @@ setup(
     python_requires='>=3.5',
     requires=['atom', 'PyQt', 'ply', 'kiwisolver'],
     install_requires=['setuptools', 'atom>=0.5.0',
-                      'kiwisolver>=1.1.0', 'ply>=3.4'],
+                      'kiwisolver>=1.0.1', 'ply>=3.4'],
     setup_requires=['cppy>=1.1.0'],
     packages=find_packages(),
     package_data={
