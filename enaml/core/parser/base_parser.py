75abafa945b1e70a609b091bdcba678bcf99fb75
frmdstryr <frmdstryr@protonmail.com>
2020-01-22 09:45:47 -0500

    Allow gen expr in decl func def

--- enaml/core/parser/base_parser.py
+++ enaml/core/parser/base_parser.py
@@ -757,7 +757,6 @@ class BaseEnamlParser(object):
         ast.FunctionDef: 'function definition',
         ast.ClassDef: 'class definition',
         ast.Yield: 'yield statement',
-        ast.GeneratorExp: 'generator expressions',
     }
 
     def _validate_decl_funcdef(self, funcdef, lexer):
