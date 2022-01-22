4cde5211530fe9f90c8f66bb063a24784fdb33ce
Matthieu Dartiailh <m.dartiailh@gmail.com>
2022-01-22 16:10:56 +0100

    tests: wip on updating tests

--- tests/core/parser/conftest.py
+++ tests/core/parser/conftest.py
@@ -1,17 +1,25 @@
-#------------------------------------------------------------------------------
-# Copyright (c) 2018, Nucleic Development Team.
+# ------------------------------------------------------------------------------
+# Copyright (c) 2018-2022, Nucleic Development Team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
 # The full license is in the file LICENSE, distributed with this software.
-#-----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+import os
+import pathlib
+
 import pytest
-from enaml.core.parser import write_tables
 
 
-@pytest.fixture(scope='session', autouse=True)
-def update_parser():
-    """Force update of the parser when running the test.
+@pytest.fixture(scope="session", autouse=True)
+def validate_parser_is_up_to_date():
+    """Check that the generated parser is up to date with its sources."""
+    from enaml.core.parser import base_enaml_parser, base_python_parser, enaml_parser
 
-    """
-    write_tables()
+    last_source_modif = max(
+        os.getmtime(base_enaml_parser.__file__),
+        os.getmtime(base_python_parser.__file__),
+        os.getmtime(pathlib.Path(base_enaml_parser.__file__).parent / "enaml.gram"),
+    )
+
+    assert os.getmtime(enaml_parser.__file__) >= last_source_modif, "Generated parser appears outdated compared to its sources, re-generate it using enaml/core/parser/generate_enaml_parser.enaml"
